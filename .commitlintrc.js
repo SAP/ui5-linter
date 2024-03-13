27f96f7a5e46c52165b891d6ff03ac3d658fc391
Matthias Osswald <mat.osswald@sap.com>
2024-03-13 13:00:29 +0100

    test(commitlint): Add exception for release commits

---
+++ .commitlintrc.js
@@ -0,0 +1,34 @@
+export default {
+	extends: [
+		"@commitlint/config-conventional",
+	],
+	rules: {
+		"type-enum": [
+			2,
+			"always",
+			[
+				"build",
+				"ci",
+				"deps",
+				"docs",
+				"feat",
+				"fix",
+				"perf",
+				"refactor",
+				"release",
+				"revert",
+				"style",
+				"test",
+			],
+		],
+		"body-max-line-length": [2, "always", 160],
+		"subject-case": [
+			2, "always",
+			["sentence-case", "start-case", "pascal-case"],
+		],
+	},
+	ignores: [
+		// Ignore release commits, as their subject doesn't start with an uppercase letter
+		(message) => message.startsWith("release: v"),
+	],
+};
