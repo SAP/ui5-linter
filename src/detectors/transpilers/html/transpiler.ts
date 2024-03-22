2b19bf6bda3af91dbed593ed1fe5d47313a2b41d
Yavor Ivanov <yavor.ivanov@sap.com>
2024-03-22 15:23:15 +0200

    feat: Add html parser

---
+++ src/detectors/transpilers/html/transpiler.ts
@@ -0,0 +1,58 @@
+import type { ReadStream } from "node:fs";
+import { Detail, SaxEventType, SAXParser, Tag as SaxTag } from "sax-wasm";
+import { finished } from "node:stream/promises";
+import fs from "node:fs/promises";
+import { createRequire } from "node:module";
+const require = createRequire(import.meta.url);
+
+let saxWasmBuffer: Buffer;
+async function initSaxWasm() {
+	if (!saxWasmBuffer) {
+		const saxPath = require.resolve("sax-wasm/lib/sax-wasm.wasm");
+		saxWasmBuffer = await fs.readFile(saxPath);
+	}
+
+	return saxWasmBuffer;
+}
+
+export async function parseHtml(contentStream: ReadStream, parseHandler: (type: SaxEventType, tag: Detail) => void) {
+	const options = { highWaterMark: 32 * 1024 }; // 32k chunks
+	const saxWasmBuffer = await initSaxWasm();
+	const saxParser = new SAXParser(SaxEventType.OpenTag | SaxEventType.CloseTag, options);
+
+	saxParser.eventHandler = parseHandler;
+
+	// Instantiate and prepare the wasm for parsing
+	if (!await saxParser.prepareWasm(saxWasmBuffer)) {
+		throw new Error("Unknown error during WASM Initialization");
+	}
+
+	// stream from a file in the current directory
+	contentStream.on("data", (chunk: Uint8Array) => {
+		try {
+			saxParser.write(chunk);
+		} catch (err) {
+			if (err instanceof Error) {
+				// In case of an error, destroy the content stream to make the
+				// error bubble up to our callers
+				contentStream.destroy(err);
+			} else {
+				throw err;
+			}
+		}
+	});
+	await finished(contentStream);
+	saxParser.end();
+}
+
+export async function extractScriptTags(contentStream: ReadStream) {
+	await parseHtml(contentStream, (event, tag) => {
+		if (tag instanceof SaxTag) {
+			if (event === SaxEventType.OpenTag) {
+				console.log(tag.value);
+			} else if (event === SaxEventType.CloseTag) {
+				console.log(tag.value);
+			}
+		}
+	});
+}
