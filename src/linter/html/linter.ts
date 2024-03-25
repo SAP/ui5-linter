27340be336a67c0d7eb059250f126fbb6e8f9f99
Yavor Ivanov <yavor.ivanov@sap.com>
2024-03-25 15:08:50 +0200

    fix: Eslint issues

--- src/linter/html/linter.ts
+++ src/linter/html/linter.ts
@@ -3,9 +3,8 @@ import {extractScriptTags} from "../../detectors/transpilers/html/parser.js";
 import {LintMessageSeverity} from "../../detectors/AbstractDetector.js";
 import Reporter from "../../detectors/Reporter.js";
 
-import type { TranspileResult } from "../../detectors/transpilers/AbstractTranspiler.js";
-import type { ReadStream } from "node:fs";
-
+import type {TranspileResult} from "../../detectors/transpilers/AbstractTranspiler.js";
+import type {ReadStream} from "node:fs";
 
 export async function lintHtml(resourceName: string, contentStream: ReadStream): Promise<TranspileResult> {
 	const taskLintEnd = taskStart("Static lint", resourceName);
@@ -14,15 +13,14 @@ export async function lintHtml(resourceName: string, contentStream: ReadStream):
 	const scriptTags = await extractScriptTags(contentStream);
 	const jsScriptTags = scriptTags.filter((tag) => tag.attributes.every((attr) => {
 		// The "type" attribute of the script tag should be
-		// 1. not set (default), 
-		// 2. an empty string, 
+		// 1. not set (default),
+		// 2. an empty string,
 		// 3. or a JavaScript MIME type (text/javascript)
 		// https://developer.mozilla.org/en-US/docs/Web/HTML/Element/script/type#attribute_is_not_set_default_an_empty_string_or_a_javascript_mime_type
 		return attr.name.value !== "type" ||
 			(attr.name.value === "type" &&
-				(attr.value.value === "" || attr.value.value === "text/javascript"));
+			(attr.value.value === "" || attr.value.value === "text/javascript"));
 	}));
-
 
 	jsScriptTags.forEach((tag) => {
 		const scriptContent = tag.textNodes?.map((tNode) => tNode.value).join("").trim();
