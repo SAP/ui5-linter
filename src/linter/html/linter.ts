d0311d0d23abf3e838f8b1362a3e44f3deef76a6
Yavor Ivanov <yavor.ivanov@sap.com>
2024-03-25 15:02:55 +0200

    feat: Implement html script linter

--- src/linter/html/linter.ts
+++ src/linter/html/linter.ts
@@ -1,4 +1,7 @@
-import { taskStart } from "../../detectors/util/perf.js";
+import {taskStart} from "../../detectors/util/perf.js";
+import {extractScriptTags} from "../../detectors/transpilers/html/transpiler.js";
+import {LintMessageSeverity} from "../../detectors/AbstractDetector.js";
+import Reporter from "../å../detectors/Reporter.js";
 
 import type { TranspileResult } from "../../detectors/transpilers/AbstractTranspiler.js";
 import type { ReadStream } from "node:fs";
@@ -6,7 +9,37 @@ import type { ReadStream } from "node:fs";
 
 export async function lintHtml(resourceName: string, contentStream: ReadStream): Promise<TranspileResult> {
 	const taskLintEnd = taskStart("Static lint", resourceName);
+	const report = new Reporter("", resourceName);
+
+	const scriptTags = await extractScriptTags(contentStream);
+	const jsScriptTags = scriptTags.filter((tag) => tag.attributes.every((attr) => {
+		// The "type" attribute of the script tag should be
+		// 1. not set (default), 
+		// 2. an empty string, 
+		// 3. or a JavaScript MIME type (text/javascript)
+		// https://developer.mozilla.org/en-US/docs/Web/HTML/Element/script/type#attribute_is_not_set_default_an_empty_string_or_a_javascript_mime_type
+		return attr.name.value !== "type" ||
+			(attr.name.value === "type" &&
+				(attr.value.value === "" || attr.value.value === "text/javascript"));
+	}));
+
+
+	jsScriptTags.forEach((tag) => {
+		const scriptContent = tag.textNodes?.map((tNode) => tNode.value).join("").trim();
+
+		if (scriptContent) {
+			report.addMessage({
+				// node: `/sap.ui5/dependencies/libs/${libKey}`,
+				severity: LintMessageSeverity.Error,
+				ruleId: "ui5-linter-csp-compliance",
+				message: `Use of inline javascript`,
+				messageDetails: "In order to avoid CSP errors, avoid usage of inline javascript",
+			});
+		}
+	});
+
 	taskLintEnd();
 
-	return { messages: [], source: "", map: "" };
+	const {messages} = report.getReport();
+	return {messages, source: "", map: ""};
 }
