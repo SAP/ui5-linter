00f4b91439230270d59c01bb89214abcf30672f7
Matthias Osswald <mat.osswald@sap.com>
2024-03-26 10:58:38 +0100

    test: Update fixtures
    Findings are already detected.

--- test/fixtures/linter/rules/NoDeprecatedApi/sap.ui.require-toplevel.js
+++ test/fixtures/linter/rules/NoDeprecatedApi/sap.ui.require-toplevel.js
@@ -1,5 +1,5 @@
 sap.ui.require(["sap/m/Button"], (Button) => {
 	new Button({
-		tap: () => console.log("Tapped") // TODO detect: Event "tap" is deprecated
+		tap: () => console.log("Tapped") // Event "tap" is deprecated
 	}).placeAt("button");
 });
