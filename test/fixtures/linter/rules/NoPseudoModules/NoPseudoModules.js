db2f5743b8d68268fddb58fc7ea34261ba1c37d1
Matthias Osswald <mat.osswald@sap.com>
2024-03-26 09:38:09 +0100

    test: Add fixtures for pseudo modules

---
+++ test/fixtures/linter/rules/NoPseudoModules/NoPseudoModules.js
@@ -0,0 +1,6 @@
+sap.ui.define([
+	"sap/ui/core/BarColor", // TODO detect: BarColor is defined in sap/ui/core/library
+	"sap/m/ListSeparators" // TODO detect: ListSeparators is defined in sap/m/library
+], function() {
+
+});
