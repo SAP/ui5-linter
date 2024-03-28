cecec75d06ffbbf15c06134a1eb4054bee834727
Yavor Ivanov <yavor.ivanov@sap.com>
2024-03-28 15:20:47 +0200

    feat: Enable new ui5lint version in console

--- src/cli.ts
+++ src/cli.ts
@@ -5,7 +5,35 @@ import {fileURLToPath} from "node:url";
 import {setVersion} from "./cli/version.js";
 import {createRequire} from "module";
 
+import type {Package} from "update-notifier";
+
 export default async function () {
+	const require = createRequire(import.meta.url);
+	const pkg = require("../package.json") as Package;
+
+	// Only import update-notifier when it's not disabled
+	// See https://github.com/yeoman/update-notifier/blob/3046d0f61a57f8270291b6ab271f8a12df8421a6/update-notifier.js#L57-L60
+	// The "is-ci" check is not executed, but will be checked by update-notifier itself then
+	const NO_UPDATE_NOTIFIER = "--no-update-notifier";
+	const disableUpdateNotifier =
+		"NO_UPDATE_NOTIFIER" in process.env ||
+		process.env.NODE_ENV === "test" ||
+		process.argv.includes(NO_UPDATE_NOTIFIER);
+
+	if (!disableUpdateNotifier) {
+		const {default: updateNotifier} = await import("update-notifier");
+		updateNotifier({
+			pkg,
+			updateCheckInterval: 86400000, // 1 day
+			shouldNotifyInNpmScript: true
+		}).notify();
+	}
+
+	// Remove --no-update-notifier from argv as it's not known to yargs, but we still want to support using it
+	if (process.argv.includes(NO_UPDATE_NOTIFIER)) {
+		process.argv = process.argv.filter((v) => v !== NO_UPDATE_NOTIFIER);
+	}
+
 	const cli = yargs(hideBin(process.argv));
 	cli.parserConfiguration({
 		"parse-numbers": false,
@@ -14,8 +42,6 @@ export default async function () {
 	// Explicitly set CLI version as the yargs default might
 	// be wrong in case a local CLI installation is used
 	// Also add CLI location
-	const require = createRequire(import.meta.url);
-	const pkg = require("../package.json") as {version: string};
 	const ui5LintJsPath = fileURLToPath(new URL("../bin/ui5lint.js", import.meta.url));
 	const pkgVersion = `${pkg.version} (from ${ui5LintJsPath})`;
 
