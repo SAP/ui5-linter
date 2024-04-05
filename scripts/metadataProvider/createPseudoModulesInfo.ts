e1ae412f6b880c2e7ae4316c1d8eacd2ccc0853d
Yavor Ivanov <yavor.ivanov@sap.com>
2024-04-05 10:21:28 +0300

    refactor: Address Eslint issues

--- scripts/metadataProvider/createPseudoModulesInfo.ts
+++ scripts/metadataProvider/createPseudoModulesInfo.ts
@@ -3,7 +3,7 @@ import {Extract} from "unzip-stream";
 import MetadataProvider from "./MetadataProvider.js";
 import {writeFile} from "node:fs/promises";
 
-import type {UI5Enum} from "@ui5-language-assistant/semantic-model-types";
+import type {UI5Enum, UI5EnumValue} from "@ui5-language-assistant/semantic-model-types";
 
 const RAW_API_JSON_FILES_FOLDER = "tmp/apiJson";
 
@@ -33,56 +33,57 @@ async function transformFiles() {
 		acc[curEnum.library].push(curEnum);
 
 		return acc;
-	}, Object.create(null));
+	}, Object.create(null) as Record<string, UI5Enum[]>);
 
 	await addOverrides(groupedEnums);
 }
 
+function buildJSDoc(enumEntry: UI5Enum | UI5EnumValue, indent = "") {
+	const jsDocBuilder: string[] = [`${indent}/**`];
+
+	if (enumEntry.description) {
+		jsDocBuilder.push(`${indent} * ${enumEntry.description.replaceAll("\n", "\n" + indent + " * ")}`);
+		jsDocBuilder.push(`${indent} *`);
+	}
+
+	if (enumEntry.experimentalInfo) {
+		let experimental = `${indent} * @experimental`;
+		if (enumEntry.experimentalInfo.since) {
+			experimental += ` (since ${enumEntry.experimentalInfo.since})`;
+		}
+		if (enumEntry.experimentalInfo.text) {
+			experimental += ` - ${enumEntry.experimentalInfo.text}`;
+		}
+		jsDocBuilder.push(experimental);
+	}
+
+	if (enumEntry.deprecatedInfo) {
+		let deprecated = `${indent} * @deprecated`;
+		if (enumEntry.deprecatedInfo.since) {
+			deprecated += ` (since ${enumEntry.deprecatedInfo.since})`;
+		}
+		if (enumEntry.deprecatedInfo.text) {
+			deprecated += ` - ${enumEntry.deprecatedInfo.text}`;
+		}
+		jsDocBuilder.push(deprecated);
+	}
+
+	if (enumEntry.visibility) {
+		jsDocBuilder.push(`${indent} * @${enumEntry.visibility}`);
+	}
+
+	if (enumEntry.since) {
+		jsDocBuilder.push(`${indent} * @since ${enumEntry.since}`);
+	}
+	jsDocBuilder.push(`${indent}*/`);
+
+	return jsDocBuilder.join("\n");
+}
+
 async function addOverrides(enums: Record<string, UI5Enum[]>) {
 	const indexFilesImports: string[] = [];
-	const buildJSDoc = (enumEntry: UI5Enum, indent = "") => {
-		const jsDocBuilder: string[] = [`${indent}/**`];
 
-		if (enumEntry.description) {
-			jsDocBuilder.push(`${indent} * ${enumEntry.description.replaceAll("\n", "\n" + indent + " * ")}`);
-			jsDocBuilder.push(`${indent} *`);
-		}
-
-		if (enumEntry.experimentalInfo) {
-			let experimental = `${indent} * @experimental`;
-			if (enumEntry.experimentalInfo.since) {
-				experimental += ` (since ${enumEntry.experimentalInfo.since})`;
-			}
-			if (enumEntry.experimentalInfo.text) {
-				experimental += ` - ${enumEntry.experimentalInfo.text}`;
-			}
-			jsDocBuilder.push(experimental);
-		}
-
-		if (enumEntry.deprecatedInfo) {
-			let deprecated = `${indent} * @deprecated`;
-			if (enumEntry.deprecatedInfo.since) {
-				deprecated += ` (since ${enumEntry.deprecatedInfo.since})`;
-			}
-			if (enumEntry.deprecatedInfo.text) {
-				deprecated += ` - ${enumEntry.deprecatedInfo.text}`;
-			}
-			jsDocBuilder.push(deprecated);
-		}
-
-		if (enumEntry.visibility) {
-			jsDocBuilder.push(`${indent} * @${enumEntry.visibility}`);
-		}
-
-		if (enumEntry.since) {
-			jsDocBuilder.push(`${indent} * @since ${enumEntry.since}`);
-		}
-		jsDocBuilder.push(`${indent}*/`);
-
-		return jsDocBuilder.join("\n");
-	};
-
-	Object.keys(enums).forEach(async (libName) => {
+	for (const libName of Object.keys(enums)) {
 		const enumEntries = enums[libName];
 
 		const stringBuilder: string[] = [
@@ -110,7 +111,7 @@ async function addOverrides(enums: Record<string, UI5Enum[]>) {
 			new URL(`../../resources/overrides/library/${libName}.d.ts`, import.meta.url),
 			stringBuilder.join("\n")
 		);
-	});
+	}
 
 	await writeFile(
 		new URL(`../../resources/overrides/library/index.d.ts`, import.meta.url),
