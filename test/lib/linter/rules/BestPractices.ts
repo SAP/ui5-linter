daeb3156e681a4a6af32df22d27d44dc7b5979fe
Yavor Ivanov <yavor.ivanov@sap.com>
2024-04-19 11:40:34 +0300

    refactor: Automatically resolve BestPractices subdirs

--- test/lib/linter/rules/BestPractices.ts
+++ test/lib/linter/rules/BestPractices.ts
@@ -1,14 +1,18 @@
-import {fileURLToPath} from "node:url";
-import {runLintRulesTests} from "../_linterHelper.js";
+import { fileURLToPath } from "node:url";
+import { runLintRulesTests } from "../_linterHelper.js";
 import path from "node:path";
+import { readdirSync, lstatSync } from "node:fs";
 
 const filePath = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(filePath);
 const fileName = path.basename(filePath, ".ts");
 const fixturesPath = path.join(__dirname, "..", "..", "..", "fixtures", "linter", "rules", fileName);
 
-runLintRulesTests(fileName, path.join(fixturesPath, "Negative_1"));
-runLintRulesTests(fileName, path.join(fixturesPath, "Negative_2"));
+const testSubDirs = readdirSync(fixturesPath);
 
-runLintRulesTests(fileName, path.join(fixturesPath, "Positive_1"));
-runLintRulesTests(fileName, path.join(fixturesPath, "Positive_2"));
+for (const subDir of testSubDirs) {
+	const dirPath = path.join(fixturesPath, subDir);
+	if (!subDir.startsWith("_") && lstatSync(dirPath).isDirectory()) {
+		runLintRulesTests(fileName, dirPath);
+	}
+}
