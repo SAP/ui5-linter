ddf092d52248ca2ca1a3526fed7b69e7861a21f0
Yavor Ivanov <yavor.ivanov@sap.com>
2024-04-25 15:49:12 +0300

    fix: Test Component's namespace

--- test/fixtures/linter/rules/BestPractices/Negative_6/Component.js
+++ test/fixtures/linter/rules/BestPractices/Negative_6/Component.js
@@ -1,4 +1,4 @@
-sap.ui.define(["com/ui5/troublesome/app/ParentComponent"], function (ParentComponent) {
+sap.ui.define(["mycomp/ParentComponent"], function (ParentComponent) {
 	"use strict";
 
 	return ParentComponent.extend("mycomp.Component", {
