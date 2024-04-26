1091024f4283f65f9e011e8e948205e633dee2d0
Yavor Ivanov <yavor.ivanov@sap.com>
2024-04-26 15:08:37 +0300

    fix: Linting test filenames

--- test/lib/linter/_linterHelper.ts
+++ test/lib/linter/_linterHelper.ts
@@ -135,7 +135,13 @@ function testDefinition(
 		});
 		assertExpectedLintResults(t, res, fixturesPath, filePaths);
 		res.forEach((results) => {
-			results.filePath = testName;
+			const chunks = testName.split("/").slice(0, -1);
+			
+			if (chunks.length > 0) {
+				results.filePath = path.join(...chunks, path.basename(results.filePath));
+			} else {
+				results.filePath = testName;	
+			}
 		});
 		t.snapshot(res);
 	});
