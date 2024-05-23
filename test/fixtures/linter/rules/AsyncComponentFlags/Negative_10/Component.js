6a868277f5e7d7a80363d2fbdd2a8aa99aa6d4fa
Merlin Beutlberger <m.beutlberger@sap.com>
2024-05-23 15:50:05 +0200

    test(AsyncComponentFlags): Add more test cases
    Snapshots reflect unexpected behavior. Negative tests should have no
    findings while positive tests should show findings.

---
+++ test/fixtures/linter/rules/AsyncComponentFlags/Negative_10/Component.js
@@ -0,0 +1,5 @@
+sap.ui.define(["mycomp/ParentComponent"], function (ParentComponent) {
+	"use strict";
+
+	return ParentComponent.extend("mycomp.Component", {});
+});
---
+++ test/fixtures/linter/rules/AsyncComponentFlags/Negative_11/Component.ts
@@ -0,0 +1,7 @@
+import ParentComponent from "mycomp/ParentComponent";
+
+export default class Component extends ParentComponent {
+	static metadata = {
+		manifest: "json",
+	};
+}
