9a3c449181cd76c1239aa51d29f861321a2bdd28
Yavor Ivanov <yavor.ivanov@sap.com>
2024-05-27 22:22:34 +0300

    refactor: Update messages

--- src/linter/linterReporting.ts
+++ src/linter/linterReporting.ts
@@ -1,12 +1,11 @@
 export const MESSAGES = {
-	SHORT__NO_DIRECT_DATATYPE_ACCESS: "Deprecated access to DataType ('{0}')",
-	DETAILS__NO_DIRECT_DATATYPE_ACCESS: "DataType can be accessed only via the static " +
-	"DataType.getType(\"{0}\") method. " +
-	"{@link topic:00737d6c1b864dc3ab72ef56611491c4 Migrating Deprecated Pseudo Modules}",
+	SHORT__NO_DIRECT_DATATYPE_ACCESS: "Deprecated access to DataType pseudo module '{0}'",
+	DETAILS__NO_DIRECT_DATATYPE_ACCESS:
+		"{@link topic:00737d6c1b864dc3ab72ef56611491c4 Migrating Access to Pseudo Modules}",
 
 	SHORT__DEPRECATED_ACCESS_ENUM: "Deprecated access to enum pseudo module '{0}'",
 	DETAILS__DEPRECATED_ACCESS_ENUM:
-		"{@link topic:00737d6c1b864dc3ab72ef56611491c4 Migrating Deprecated Pseudo Modules}",
+		"{@link topic:00737d6c1b864dc3ab72ef56611491c4 Migrating Access to Pseudo Modules}",
 
 	SHORT__DEPRECATED_PROP_OF_CLASS: "Use of deprecated property '{0}' of class '{1}'",
 
@@ -25,7 +24,7 @@ export const MESSAGES = {
 };
 
 // TODO: Migrate to enum instead of Object/Map
-// Currently, it's done this way to avoid polution of the test snapshots
+// Currently, it's done this way to avoid pollution of the test snapshots
 export const RULES = {
 	"ui5-linter-no-deprecated-api": "ui5-linter-no-deprecated-api",
 	"ui5-linter-no-partially-deprecated-api": "ui5-linter-no-partially-deprecated-api",
