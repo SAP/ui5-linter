e69607f920845ee06ac1ee2dec2bc95d92330bf7
Merlin Beutlberger <m.beutlberger@sap.com>
2024-07-01 17:10:59 +0200

    feat: [WIP] Transform any ManagedObject#extend call to ES class

--- test/fixtures/transpiler/amd/Factory_ExtendNameConflict.js
+++ test/fixtures/transpiler/amd/Factory_ExtendNameConflict.js
@@ -1,5 +1,7 @@
 sap.ui.define(["sap/ui/core/UIComponent", `sap/ui/Device`], function (UIComponent) {
 	"use strict";
 
+	// Given class name "[...].UIComponent" is identical to an existing variable and therefore must not
+	// be used for the generated ES class
 	return UIComponent.extend("com.ui5.troublesome.app.UIComponent", {});
 });
