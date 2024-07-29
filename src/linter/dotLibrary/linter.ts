a876b144b3ffbb5e2504af2d240410b504726088
Yavor Ivanov <yavor.ivanov@sap.com>
2024-07-29 11:55:13 +0300

    fix: Use workspace reader instead of the rootReader

--- src/linter/dotLibrary/linter.ts
+++ src/linter/dotLibrary/linter.ts
@@ -2,24 +2,23 @@ import {LinterParameters} from "../LinterContext.js";
 import DotLibraryLinter from "./DotLibraryLinter.js";
 import {Resource} from "@ui5/fs";
 
-export default async function lintDotLibrary({context}: LinterParameters) {
+export default async function lintDotLibrary({context, workspace}: LinterParameters) {
 	let dotLibraryResources: Resource[];
 	const pathsToLint = context.getPathsToLint();
-	const reader = context.getRootReader();
 	if (pathsToLint?.length) {
 		dotLibraryResources = [];
 		await Promise.all(pathsToLint.map(async (resourcePath) => {
 			if (!resourcePath.endsWith(".library")) {
 				return;
 			}
-			const resource = await reader.byPath(resourcePath);
+			const resource = await workspace.byPath(resourcePath);
 			if (!resource) {
 				throw new Error(`Resource not found: ${resourcePath}`);
 			}
 			dotLibraryResources.push(resource);
 		}));
 	} else {
-		dotLibraryResources = await reader.byGlob("/src/**/.library");
+		dotLibraryResources = await workspace.byGlob("/src/**/.library");
 	}
 
 	await Promise.all(dotLibraryResources.map(async (resource: Resource) => {
