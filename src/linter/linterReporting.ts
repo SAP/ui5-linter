786e972162a84678d0cd5fd0fb60bccafcf534d7
Matthias Osswald <mat.osswald@sap.com>
2024-08-09 11:17:30 +0200

    refactor: Adjust more messages

--- src/linter/linterReporting.ts
+++ src/linter/linterReporting.ts
@@ -1,29 +1,10 @@
 import type {MESSAGE_INFO} from "./messages.js";
 
 export const MESSAGES = {
-	SHORT__NO_DIRECT_DATATYPE_ACCESS: "Deprecated access to DataType pseudo module '{0}'",
-	DETAILS__NO_DIRECT_DATATYPE_ACCESS:
-		"{@link topic:00737d6c1b864dc3ab72ef56611491c4 Migrating Access to Pseudo Modules}",
-
-	SHORT__DEPRECATED_ACCESS_ENUM: "Deprecated access to enum pseudo module '{0}'",
-	DETAILS__DEPRECATED_ACCESS_ENUM:
-		"{@link topic:00737d6c1b864dc3ab72ef56611491c4 Migrating Access to Pseudo Modules}",
-
-	SHORT__DEPRECATED_PROP_OF_CLASS: "Use of deprecated property '{0}' of class '{1}'",
-
+	// Used by ManifestLinter
 	SHORT__DEPRECATED_PROP: "Use of deprecated property {0}",
 
-	SHORT__DEPRECATED_FUNCTION_ACCESS: "Call to deprecated function {0}",
-
-	SHORT__DEPRECATED_API_ACCESS: "Use of deprecated API '{0}'",
-
-	SHORT__DEPRECATED_PROP_ACCESS: "Access of deprecated property '{0}'",
-
-	SHORT__DEPRECATED_MODULE_IMPORT: "Import of deprecated module '{0}'",
-
 	SHORT__DEPRECATED_COMPONENT: "Use of deprecated component '{0}'",
-
-	SHORT__GLOBAL_VAR_ACCESS: "Access of global variable '{0}' ({1})",
 
 	SHORT__LIB_INIT_2: "Call to {0}() must be declared with property {apiVersion: 2}",
 	DETAILS__LIB_INIT_2: "{@link sap.ui.core.Lib.init Lib.init}",
@@ -56,7 +37,7 @@ export function formatMessage(message: string, ...params: string[]) {
 
 export type MessageInfo = typeof MESSAGE_INFO[keyof typeof MESSAGE_INFO] & {fatal?: boolean};
 
-type ExtractArgs<F> = F extends (args: infer P) => string ? P : never;
+type ExtractArgs<F> = F extends (args: infer P) => unknown ? P : never;
 type CombineArgs<M, D> = M & D extends object ? M & D : never;
 
 export type MessageArgs = {
--- src/linter/messages.ts
+++ src/linter/messages.ts
@@ -2,15 +2,89 @@ import {LintMessageSeverity} from "./LinterContext.js";
 import {RULES} from "./linterReporting.js";
 
 export enum MESSAGE {
+	DEPRECATED_API_ACCESS,
+	DEPRECATED_FUNCTION_CALL,
+	DEPRECATED_MODULE_IMPORT,
+	DEPRECATED_PROPERTY_OF_CLASS,
+	DEPRECATED_PROPERTY,
 	NO_DIRECT_DATATYPE_ACCESS,
+	NO_DIRECT_ENUM_ACCESS,
+	NO_GLOBALS,
 }
 export const MESSAGE_INFO = {
+
+	[MESSAGE.DEPRECATED_API_ACCESS]: {
+		severity: LintMessageSeverity.Error,
+		ruleId: RULES["ui5-linter-no-deprecated-api"],
+
+		message: ({apiName}: {apiName: string}) =>
+			`Use of deprecated API '${apiName}'`,
+		details: ({details}: {details: string}) => details,
+	},
+
+	[MESSAGE.DEPRECATED_FUNCTION_CALL]: {
+		severity: LintMessageSeverity.Error,
+		ruleId: RULES["ui5-linter-no-deprecated-api"],
+
+		message: ({functionName, additionalMessage}: {functionName: string; additionalMessage: string}) =>
+			`Call to deprecated function '${functionName}'${additionalMessage ? ` ${additionalMessage}` : ""}`,
+		details: ({details}: {details: string}) => details,
+	},
+
+	[MESSAGE.DEPRECATED_MODULE_IMPORT]: {
+		severity: LintMessageSeverity.Error,
+		ruleId: RULES["ui5-linter-no-deprecated-api"],
+
+		message: ({moduleName}: {moduleName: string}) =>
+			`Import of deprecated module '${moduleName}'`,
+		details: ({details}: {details: string}) => details,
+	},
+
+	[MESSAGE.DEPRECATED_PROPERTY_OF_CLASS]: {
+		severity: LintMessageSeverity.Error,
+		ruleId: RULES["ui5-linter-no-deprecated-api"],
+
+		message: ({propertyName, className}: {propertyName: string; className: string}) =>
+			`Use of deprecated property '${propertyName}' of class '${className}'`,
+		details: ({details}: {details: string}) => details,
+	},
+
+	[MESSAGE.DEPRECATED_PROPERTY]: {
+		severity: LintMessageSeverity.Error,
+		ruleId: RULES["ui5-linter-no-deprecated-property"],
+
+		message: ({propertyName}: {propertyName: string}) =>
+			`Use of deprecated property '${propertyName}'`,
+		details: ({details}: {details: string}) => details,
+	},
+
 	[MESSAGE.NO_DIRECT_DATATYPE_ACCESS]: {
 		severity: LintMessageSeverity.Error,
 		ruleId: RULES["ui5-linter-no-pseudo-modules"],
+
 		message: ({moduleName}: {moduleName: string}) =>
 			`Deprecated access to DataType pseudo module '${moduleName}'`,
 		details: () =>
 			"{@link topic:00737d6c1b864dc3ab72ef56611491c4 Migrating Access to Pseudo Modules}",
 	},
+
+	[MESSAGE.NO_DIRECT_ENUM_ACCESS]: {
+		severity: LintMessageSeverity.Error,
+		ruleId: RULES["ui5-linter-no-pseudo-modules"],
+
+		message: ({moduleName}: {moduleName: string}) =>
+			`Deprecated access to enum pseudo module '${moduleName}'`,
+		details: () =>
+			"{@link topic:00737d6c1b864dc3ab72ef56611491c4 Migrating Access to Pseudo Modules}",
+	},
+
+	[MESSAGE.NO_GLOBALS]: {
+		severity: LintMessageSeverity.Error,
+		ruleId: RULES["ui5-linter-no-globals-js"],
+
+		message: ({variableName, namespace}: {variableName: string; namespace: string}) =>
+			`Access of global variable '${variableName}' (${namespace})`,
+		details: () => null,
+	},
+
 } as const;
