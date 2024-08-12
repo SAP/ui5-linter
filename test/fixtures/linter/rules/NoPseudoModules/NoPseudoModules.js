b0419704aea378633e1b6c4dc5c642cb6b9adebf
Matthias Osswald <mat.osswald@sap.com>
2024-08-12 11:03:50 +0200

    test: Remove obsolete TODO
    CSSSize is already detected

--- test/fixtures/linter/rules/NoPseudoModules/NoPseudoModules.js
+++ test/fixtures/linter/rules/NoPseudoModules/NoPseudoModules.js
@@ -1,7 +1,7 @@
 sap.ui.define([
 	"sap/ui/core/BarColor", // BarColor is defined in sap/ui/core/library
 	"sap/m/ListSeparators", // ListSeparators is defined in sap/m/library
-	"sap/ui/core/CSSSize" // TODO detect: CSSSize is defined in sap/ui/core/library
+	"sap/ui/core/CSSSize" // CSSSize is defined in sap/ui/core/library
 ], function() {
 
 });
