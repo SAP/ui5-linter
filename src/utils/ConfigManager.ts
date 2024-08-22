7d2e2acb0caa0ce4a757561b704eddf90a40e3b0
Yavor Ivanov <yavor.ivanov@sap.com>
2024-08-22 14:20:43 +0300

    refactor: Resolve paths to posix on all OS

--- src/utils/ConfigManager.ts
+++ src/utils/ConfigManager.ts
@@ -21,13 +21,22 @@ export default class ConfigManager {
 		this.#configFile = configFile ?? null;
 	}
 
+	#resolveModulePaths(fileName: string): string {
+		// Node on Windows behaves strange, does not work with absolute paths
+		// and modifies files extensions in tests i.e. js -> ts, mjs -> tjs
+		// Keeping the relative path in POSIX format resolves those issues.
+		return path.posix.join(
+			path.relative(__dirname, this.#cwd).replaceAll(path.win32.sep, path.posix.sep),
+			fileName);
+	}
+
 	async getConfiguration(): Promise<UI5LintConfigType[]> {
 		const configs: UI5LintConfigType[] = [];
 		let config: UI5LintConfigType | undefined;
 
 		if (this.#configFile) {
 			// Relative paths are needed to make it work on Windows
-			const configFilePath = path.join(path.relative(__dirname, this.#cwd), this.#configFile).replace(/ts$/, "js");
+			const configFilePath = this.#resolveModulePaths(this.#configFile);
 			({default: config} = await import(configFilePath) as {default: UI5LintConfigType});
 		} else {
 			// Find configuration file
@@ -35,7 +44,7 @@ export default class ConfigManager {
 				CONFIG_FILENAMES.map(
 					(filename) => {
 						// Relative paths are needed to make it work on Windows
-						const configFilePath = path.join(path.relative(__dirname, this.#cwd), filename).replace(/ts$/, "js");
+						const configFilePath = this.#resolveModulePaths(filename);
 						return import(configFilePath) as Promise<{default: UI5LintConfigType}>;
 					}))
 				// Promise.any would throw if nothing is found i.e. there's no config file
