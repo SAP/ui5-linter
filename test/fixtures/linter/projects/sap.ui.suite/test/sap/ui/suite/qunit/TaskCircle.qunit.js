4e61ab096bc2dfac55457a16d69b8a4abc9a04e5
Matthias Osswald <mat.osswald@sap.com>
2024-09-13 13:51:46 +0200

    test: Add fixture for sap.ui.suite project
    There are some special circumstances when linting sap.ui.suite which
    were detected by manual testing.
    Some findings were only reported with a newer version of UI5 linter.
    
    It turned out that https://github.com/SAP/ui5-linter/pull/290 changed
    the behavior and fixed the reporting of deprecations within the same
    library, as previously the @sapui5/types were not loaded properly.
    
    It is still unknown why exactly this was happening, but it is fixed now
    and the added test project should ensure that no similar issues will
    appear in the future.

---
+++ test/fixtures/linter/projects/sap.ui.suite/test/sap/ui/suite/qunit/TaskCircle.qunit.js
@@ -0,0 +1,9 @@
+sap.ui.define([
+	"sap/ui/suite/TaskCircle",
+	"sap/m/DateTimeInput"
+], function(TaskCircle, DateTimeInput) {
+	"use strict";
+	// The linter used to only detect the DateTimeInput deprecation in this file.
+	// It has been solved via https://github.com/SAP/ui5-linter/pull/290.
+	// This test ensures that it continues to work.
+});
