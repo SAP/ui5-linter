e7fc643662c17e19c80dec4f8ae9ce69f883202b
Max Reichmann <max.reichmann@sap.com>
2024-09-24 19:47:56 +0200

    refactor: Reuse file types

--- src/linter/fileTypes/linter.ts
+++ src/linter/fileTypes/linter.ts
@@ -2,18 +2,24 @@ import {Resource} from "@ui5/fs";
 import {LinterParameters} from "../LinterContext.js";
 import {MESSAGE} from "../messages.js";
 
+const deprecatedViewFileTypes = [
+	".view.json",
+	".view.html",
+	".view.js",
+	".view.tmpl",
+	".view.ts",
+	".fragment.html",
+	".fragment.js",
+	".fragment.ts",
+];
+
 export default async function lintFileTypes({workspace, context}: LinterParameters) {
 	let xmlResources: Resource[];
 	const pathsToLint = context.getPathsToLint();
 	if (pathsToLint?.length) {
-		// TODO: Refactor to utiliize reuse
 		xmlResources = [];
 		await Promise.all(pathsToLint.map(async (resourcePath) => {
-			if (!resourcePath.endsWith(".view.json") && !resourcePath.endsWith(".view.html") &&
-				!resourcePath.endsWith(".view.js") && !resourcePath.endsWith(".view.tmpl") &&
-				!resourcePath.endsWith(".view.ts") && !resourcePath.endsWith(".fragment.html") &&
-				!resourcePath.endsWith(".fragment.js") && !resourcePath.endsWith(".fragment.ts")
-			) {
+			if (!deprecatedViewFileTypes.some(type => resourcePath.endsWith(type))) {
 				return;
 			}
 			const resource = await workspace.byPath(resourcePath);
@@ -23,7 +29,7 @@ export default async function lintFileTypes({workspace, context}: LinterParamete
 			xmlResources.push(resource);
 		}));
 	} else {
-		xmlResources = await workspace.byGlob("**/{*.view.json,*.view.html,*.view.js,*.view.tmpl,*.view.ts,*.fragment.html,*.fragment.js,*fragment.ts}");
+		xmlResources = await workspace.byGlob(`**/{${deprecatedViewFileTypes.map(type => `*${type}`).join(",")}}`);
 	}
 
 	xmlResources.forEach((resource: Resource) => {
