c9a3ab2f53c24ef981f63086b2ec5aa2b68f8753
Matthias Osswald <mat.osswald@sap.com>
2024-10-22 10:14:03 +0200

    test: Add metadata to sap.ui.core base classes
    This ensures that the check introduced with
    https://github.com/SAP/ui5-linter/pull/371 is actually covered via the
    tests.

--- test/fixtures/linter/projects/sap.ui.core/src/sap/ui/base/ManagedObject.js
+++ test/fixtures/linter/projects/sap.ui.core/src/sap/ui/base/ManagedObject.js
@@ -1,5 +1,10 @@
 sap.ui.define(["./EventProvider"], function(EventProvider) {
 	"use strict";
-	var ManagedObject = EventProvider.extend("sap.ui.base.ManagedObject", {});
+	var ManagedObject = EventProvider.extend("sap.ui.base.ManagedObject", {
+		metadata : {
+			"abstract" : true,
+			library : "sap.ui.core", // UI Library that contains this class
+		},
+	});
 	return ManagedObject;
 });
--- test/fixtures/linter/projects/sap.ui.core/src/sap/ui/core/Control.js
+++ test/fixtures/linter/projects/sap.ui.core/src/sap/ui/core/Control.js
@@ -1,5 +1,13 @@
 sap.ui.define(["./Element"], function(Element) {
 	"use strict";
-	var Control = Element.extend("sap.ui.core.Control", {});
+	var Control = Element.extend("sap.ui.core.Control", {
+		metadata : {
+			stereotype : "control",
+			"abstract" : true,
+			library: "sap.ui.core",
+		},
+
+		renderer : null // Control has no renderer
+	});
 	return Control;
 });
