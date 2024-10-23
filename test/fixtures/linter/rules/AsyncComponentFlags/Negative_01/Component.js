3f2efb44cb96dc38a987b4c579ea1c60d0a036ca
Matthias Osswald <mat.osswald@sap.com>
2024-10-23 16:32:52 +0200

    fix: Parent class check for Component analysis
    Before this change, all classes within a Component.js/ts file were
    checked for component best practices.
    This change limits the checks to sub-classes of sap/ui/core/Component or
    sap/ui/core/UIComponent respectively.

--- test/fixtures/linter/rules/AsyncComponentFlags/Negative_01/Component.js
+++ test/fixtures/linter/rules/AsyncComponentFlags/Negative_01/Component.js
@@ -1,7 +1,10 @@
 // Fixture description:
-// IAsyncContentCreation interface is implemented, no redundant async flags in manifest.json
-sap.ui.define(["sap/ui/core/UIComponent"], function (UIComponent) {
+// - IAsyncContentCreation interface is implemented, no redundant async flags in manifest.json
+// - Component module contains additional local class (MyType) that does not inherit from the UI5 Component
+sap.ui.define(["sap/ui/core/UIComponent", "sap/ui/model/type/Unit"], function (UIComponent, UnitType) {
 	"use strict";
+
+	const MyType = UnitType.extend("mycomp.MyType", {});
 
 	return UIComponent.extend("mycomp.Component", {
 		metadata: {
