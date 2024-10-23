b8af1ed91779ba7f23a84d45a2e552a4b8f76125
Matthias Osswald <mat.osswald@sap.com>
2024-10-23 17:49:08 +0200

    test: Ensure ManagedObject checks in components
    This ensures that the ManagedObject metadata checks are also performed
    for sub-classes of a Component, which inherits from ManagedObject.

--- test/fixtures/linter/rules/AsyncComponentFlags/Positive_10/Component.js
+++ test/fixtures/linter/rules/AsyncComponentFlags/Positive_10/Component.js
@@ -1,9 +1,17 @@
 // Component which does not inherit from UIComponent:
-// Missing reference to manifest.json should be reported
+// - Missing reference to manifest.json should be reported
+// - Usage of deprecated type in properties should be reported
 sap.ui.define(["sap/ui/core/Component"], function (Component) {
 	"use strict";
 
 	return Component.extend("mycomp.Component", {
-		"metadata": {},
+		metadata: {
+			"properties": {
+				// Usage of deprecated type
+				propertyWithDeprecatedType: {
+					type: "sap.m.DateTimeInputType"
+				},
+			}
+		},
 	});
 });
