88756070988315a2bdb400ef728a83d7012912fa
Yavor Ivanov <yavor.ivanov@sap.com>
2024-11-06 19:48:10 +0200

    test: Tests for NodeAPI

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,20 @@
 import {lintProject} from "./linter/linter.js";
 import type {LintResult} from "./linter/LinterContext.js";
 
-export {LintResult} from "./linter/LinterContext.js";
-export {UI5LintConfigType} from "./utils/ConfigManager.js";
+export type {LintResult} from "./linter/LinterContext.js";
+export type {UI5LintConfigType} from "./utils/ConfigManager.js";
 
 // Define a separate interface for the Node API as there could be some differences
 // in the options and behavior compared to LinterOptions internal type.
 export interface UI5LintOptions {
 	filePatterns?: string[];
-	ignorePatterns: string[];
-	details: boolean;
+	ignorePatterns?: string[];
+	details?: boolean;
 	config?: string;
 	noConfig?: boolean;
-	coverage: boolean; // boolean(default: false)
-	ui5Config: string | object;
-	rootDir: string;
+	coverage?: boolean; // boolean(default: false)
+	ui5Config?: string | object;
+	rootDir?: string;
 }
 
 export async function ui5lint({
@@ -22,6 +22,7 @@ export async function ui5lint({
 	ignorePatterns = [],
 	details = false,
 	config,
+	noConfig,
 	coverage = false,
 	ui5Config = "./ui5.yaml",
 	rootDir = process.cwd(),
@@ -33,6 +34,7 @@ export async function ui5lint({
 		reportCoverage: coverage,
 		includeMessageDetails: details,
 		configPath: config,
+		noConfig,
 		ui5Config,
 	});
 }
---
+++ test/lib/index.ts
@@ -0,0 +1,91 @@
+import anyTest, {TestFn} from "ava";
+import sinonGlobal, {SinonStub} from "sinon";
+import esmock from "esmock";
+import path from "node:path";
+import {fileURLToPath} from "node:url";
+import {
+	esmockDeprecationText, preprocessLintResultsForSnapshot,
+} from "./linter/_linterHelper.js";
+import {LintResult} from "../../src/linter/LinterContext.js";
+import {UI5LintOptions} from "../../src/index.js";
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+const fixturesBasePath = path.join(__dirname, "..", "fixtures", "linter");
+const fixturesProjectsPath = path.join(fixturesBasePath, "projects");
+
+const test = anyTest as TestFn<{
+	sinon: sinonGlobal.SinonSandbox;
+	ui5lint: SinonStub<[UI5LintOptions], Promise<LintResult[]>>;
+}>;
+
+test.before(async (t) => {
+	t.context.sinon = sinonGlobal.createSandbox();
+
+	const {ui5lint} = await esmock("../../src/index.js", {
+		"../../src/linter/linter.js": await esmockDeprecationText(),
+	});
+
+	t.context.ui5lint = ui5lint;
+});
+test.after.always((t) => {
+	t.context.sinon.restore();
+});
+
+// Test project fixtures individually
+test.serial("ui5lint API: All files of com.ui5.troublesome.app", async (t) => {
+	const projectPath = path.join(fixturesProjectsPath, "com.ui5.troublesome.app");
+	const {ui5lint} = t.context;
+
+	const res = await ui5lint({
+		rootDir: projectPath,
+		noConfig: true,
+		ui5Config: {
+			specVersion: "3.0",
+			metadata: {
+				name: "com.ui5.troublesome.app",
+			},
+			type: "application",
+			framework: {
+				name: "OpenUI5",
+				version: "1.121.0",
+				libraries: [
+					{name: "sap.m"},
+					{name: "sap.ui.core"},
+					{name: "sap.landvisz"},
+				],
+			},
+		},
+	});
+
+	t.snapshot(preprocessLintResultsForSnapshot(res));
+});
+
+test.serial("ui5lint API: Only /webapp folder from com.ui5.troublesome.app (without details / coverage)", async (t) => {
+	const projectPath = path.join(fixturesProjectsPath, "com.ui5.troublesome.app");
+	const filePaths = [
+		// Minimatch requires POSIX
+		"webapp/",
+	];
+	const {ui5lint} = t.context;
+
+	const res = await ui5lint({
+		rootDir: projectPath,
+		filePatterns: filePaths,
+	});
+
+	t.snapshot(preprocessLintResultsForSnapshot(res));
+});
+
+test.serial("ui5lint API: com.ui5.troublesome.app with unmatched patterns", async (t) => {
+	const projectPath = path.join(fixturesProjectsPath, "com.ui5.troublesome.app");
+
+	const {ui5lint} = t.context;
+
+	await t.throwsAsync(ui5lint({
+		rootDir: projectPath,
+		config: "ui5lint.config.unmatched-patterns.mjs",
+	}), {
+		message: `Specified file patterns 'unmatched-pattern1', ` +
+			`'unmatched-pattern2', 'unmatched-pattern3' did not match any resource`,
+	});
+});
