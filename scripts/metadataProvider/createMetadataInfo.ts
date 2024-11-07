11440c500e5cfebcf01067325729be6f99a3763d
Matthias Osswald <mat.osswald@sap.com>
2024-11-07 15:30:31 +0100

    refactor: Update api-extract logic for deprecations
    This is the starting point of a larger refactoring to optimize the
    structure of the api-extract.json file and the consumption of the
    included info in the linter code.
    
    This change focuses on extracting the same information during the
    existing iteration over all symbols so that information such as
    default aggregation, deprecation text and more can be stored at the
    symbol rather than having to repeat symbol names for every kind of
    information that needs to be accessed.
    
    It also adds missing deprecation texts for some cases.

--- scripts/metadataProvider/createMetadataInfo.ts
+++ scripts/metadataProvider/createMetadataInfo.ts
@@ -4,19 +4,27 @@ import MetadataProvider from "./MetadataProvider.js";
 import {
 	forEachSymbol,
 } from "@ui5-language-assistant/semantic-model";
-import {
+import type {
+	BaseUI5Node,
 	UI5Class,
 	UI5Enum,
 	UI5Namespace,
-	UI5Interface,
-	UI5Typedef,
-	UI5Function,
-	UI5SemanticModel,
 } from "@ui5-language-assistant/semantic-model-types";
-type ui5UnionType = UI5Class | UI5Enum | UI5Namespace | UI5Interface | UI5Typedef | UI5Function;
+
 const hasFieldsProperty = function (type: unknown): type is UI5Class | UI5Enum | UI5Namespace {
 	return (type as UI5Class | UI5Enum | UI5Namespace).fields !== undefined;
 };
+
+function getDeprecationText(deprecatedInfo: BaseUI5Node["deprecatedInfo"]): string | null {
+	if (!deprecatedInfo?.isDeprecated) {
+		return null;
+	}
+	const text = (deprecatedInfo.since ? `(since ${deprecatedInfo.since}) ` : "") +
+		(deprecatedInfo.text ? `${deprecatedInfo.text}` : "");
+
+	// If the text is empty, return null so that a default value can be used
+	return text || null;
+}
 
 export default async function createMetadataInfo(apiJsonsRoot: string, sapui5Version: string) {
 	const metadataProvider = new MetadataProvider();
@@ -24,11 +32,60 @@ export default async function createMetadataInfo(apiJsonsRoot: string, sapui5Ver
 
 	const semanticModel = metadataProvider.getModel();
 
+	const deprecations: Record<string, Record<string, string>> = {
+		classes: {},
+		enums: {},
+		functions: {},
+		namespaces: {},
+		typedefs: {},
+		interfaces: {},
+	};
+
 	const defaultAggregations: Record<string, string> = {};
 	forEachSymbol(semanticModel, (symbol, symbolName) => {
 		const defaultAggregation = metadataProvider.getDefaultAggregationForSymbol(symbol);
 		if (defaultAggregation) {
 			defaultAggregations[symbolName] = defaultAggregation;
+		}
+
+		let modelName;
+		switch (symbol.kind) {
+			case "UI5Class":
+				modelName = "classes";
+				break;
+			case "UI5Enum":
+				modelName = "enums";
+				break;
+			case "UI5Interface":
+				modelName = "interfaces";
+				break;
+			case "UI5Namespace":
+				modelName = "namespaces";
+				break;
+			case "UI5Typedef":
+				modelName = "typedefs";
+				break;
+			case "UI5Function":
+				modelName = "functions";
+				break;
+			default:
+				modelName = "unknown-" + symbol.kind;
+		}
+
+		if (symbol.deprecatedInfo?.isDeprecated) {
+			const deprecationText = getDeprecationText(symbol.deprecatedInfo) ?? "deprecated";
+			deprecations[modelName] = deprecations[modelName] ?? {};
+			deprecations[modelName][symbolName] = deprecationText;
+		}
+
+		if (hasFieldsProperty(symbol)) {
+			symbol.fields?.forEach((field) => {
+				if (field?.deprecatedInfo?.isDeprecated) {
+					deprecations[modelName] = deprecations[modelName] ?? {};
+					const deprecationText = getDeprecationText(field.deprecatedInfo) ?? "deprecated";
+					deprecations[modelName][symbolName + "." + field.name] = deprecationText;
+				}
+			});
 		}
 	});
 
@@ -38,40 +95,11 @@ export default async function createMetadataInfo(apiJsonsRoot: string, sapui5Ver
 			version: sapui5Version,
 		},
 		defaultAggregations,
-		deprecations: createDeprecationsInfo(semanticModel),
+		deprecations,
 	};
 
 	await writeFile(
 		new URL("../../resources/api-extract.json", import.meta.url),
 		JSON.stringify(apiExtract, null, 2) + "\n"
 	);
-}
-
-function createDeprecationsInfo(semanticModel: UI5SemanticModel) {
-	const deprecations: Record<string, Record<string, string>> = {};
-	for (const [modelName, model] of Object.entries(semanticModel)) {
-		if (typeof model !== "object" || Array.isArray(model)) {
-			continue;
-		}
-
-		deprecations[modelName] = deprecations[modelName] ?? {};
-
-		for (const [typeName, type] of Object.entries(model as Record<string, ui5UnionType>)) {
-			if (type?.deprecatedInfo?.isDeprecated) {
-				deprecations[modelName][typeName] =
-					(type.deprecatedInfo.since ? `(since ${type.deprecatedInfo.since}) ` : "") +
-					(type.deprecatedInfo.text ? `${type.deprecatedInfo.text}` : "");
-			}
-
-			if (hasFieldsProperty(type)) {
-				type.fields?.forEach((field) => {
-					if (field?.deprecatedInfo?.isDeprecated) {
-						deprecations[modelName][typeName + "." + field.name] = "deprecated";
-					}
-				});
-			}
-		}
-	}
-
-	return deprecations;
 }
