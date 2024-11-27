SAP/ui5-linter

commit cc2cf60b9290c564e1ba6a66f47e6e9a8009d264

feat: Add type support for "byId" in controllers (#423)
This feature adds type definitions for `byId` within controllers so that
the usage of deprecated functionality can be detected when controls or
elements from the view are accessed by their ID.
Previously, only the base element class was known, which is still the
case as a fallback.

JIRA: CPOUI5FOUNDATION-843

test/fixtures/linter/projects/com.ui5.troublesome.app/webapp/controller/Main.controller.js
@@ -4,6 +4,32 @@ sap.ui.define(["./BaseController", "sap/m/MessageBox"], function (BaseController
 	return BaseController.extend("com.ui5.troublesome.app.controller.Main", {
 		sayHello: function () {
 			MessageBox.show("Hello World!");
+		},
+
+		registerButtonEventHandlers() {
+			// this.byId and this.getView().byId should report the same issues
+			this.byId("helloButton").attachTap(function() {
+				console.log("Tapped");
+			});
+			this.getView().byId("helloButton").attachTap(function() {
+				console.log("Tapped");
+			});
+
+			// testButton exists in two views and could be a sap.m.Button or a sap.ui.commons.Button.
+			// The detection of deprecated button API depends requires TypeScript compliant probing (e.g. using "attachTap" in testButton).
+			// In any case, the detection of UI5 Base Control API should still work as both inherit from it.
+			const testButton = this.byId("testButton");
+			testButton.getBlocked(); // This should be reported
+			if ("attachTap" in testButton) {
+				// When probing for the existence of the method, the type can be determined and the issue should be reported
+				testButton.attachTap(function() {
+					console.log("Tapped");
+				});
+			}
+
+			// UI5 Element API should still be checked for unknown IDs
+			this.byId("unknown").prop("foo", "bar");
+			this.getView().byId("unknown").prop("foo", "bar");
 		}
 	});
 });

src/linter/ui5Types/TypeLinter.ts
@@ -8,6 +8,7 @@ import path from "node:path/posix";
 import {AbstractAdapter} from "@ui5/fs";
 import {createAdapter, createResource} from "@ui5/fs/resourceFactory";
 import {loadApiExtract} from "../../utils/ApiExtract.js";
+import {CONTROLLER_BY_ID_DTS_PATH} from "../xmlTemplate/linter.js";
 
 const log = getLogger("linter:ui5Types:TypeLinter");
 
@@ -151,13 +152,22 @@ export default class TypeChecker {
 						resourcePath, fileContent, sourceMap);
 				}
 			}
+			// Although not being a typical transformed source, write out the byId dts file for debugging purposes
+			let byIdDts = files.get(CONTROLLER_BY_ID_DTS_PATH);
+			if (byIdDts) {
+				if (typeof byIdDts === "function") {
+					byIdDts = byIdDts();
+				}
+				await writeTransformedSources(process.env.UI5LINT_WRITE_TRANSFORMED_SOURCES,
+					CONTROLLER_BY_ID_DTS_PATH, byIdDts);
+			}
 		}
 	}
 }
 
 async function writeTransformedSources(fsBasePath: string,
 	originalResourcePath: string,
-	source: string, map: string | undefined) {
+	source: string, map?: string) {
 	const transformedWriter = createAdapter({
 		fsBasePath,
 		virBasePath: "/",
@@ -165,7 +175,7 @@ async function writeTransformedSources(fsBasePath: string,
 
 	await transformedWriter.write(
 		createResource({
-			path: originalResourcePath + ".ui5lint.transformed.js",
+			path: originalResourcePath,
 			string: source,
 		})
 	);
@@ -173,7 +183,7 @@ async function writeTransformedSources(fsBasePath: string,
 	if (map) {
 		await transformedWriter.write(
 			createResource({
-				path: originalResourcePath + ".ui5lint.transformed.js.map",
+				path: originalResourcePath + ".map",
 				string: JSON.stringify(JSON.parse(map), null, "\t"),
 			})
 		);

src/linter/xmlTemplate/ControllerByIdInfo.ts
@@ -1,0 +1,38 @@
+// Multiple views/fragments can use the same ID and link to the same controller,
+// so we need to store a set of module names for each ID.
+export type IdModulesMap = Map<string, Set<string>>;
+
+type ControllerElementsMap = Map<string, IdModulesMap>;
+
+export default class ControllerByIdInfo {
+	private map: ControllerElementsMap = new Map();
+
+	private getControllerMapping(controllerName: string) {
+		let controllerMap = this.map.get(controllerName);
+		if (!controllerMap) {
+			controllerMap = new Map();
+			this.map.set(controllerName, controllerMap);
+		}
+		return controllerMap;
+	}
+
+	public addMappings(controllerName: string, idModuleMap: Map<string, string>) {
+		// Do not add empty mappings
+		if (!idModuleMap.size) {
+			return;
+		}
+		const controllerMapping = this.getControllerMapping(controllerName);
+		for (const [id, module] of idModuleMap) {
+			let existingModules = controllerMapping.get(id);
+			if (!existingModules) {
+				existingModules = new Set();
+				controllerMapping.set(id, existingModules);
+			}
+			existingModules.add(module);
+		}
+	}
+
+	public getMappings() {
+		return this.map;
+	}
+}

src/linter/xmlTemplate/Parser.ts
@@ -9,6 +9,7 @@ import AbstractGenerator from "./generator/AbstractGenerator.js";
 import {getLogger} from "@ui5/logger";
 import {MESSAGE} from "../messages.js";
 import {ApiExtract} from "../../utils/ApiExtract.js";
+import ControllerByIdInfo from "./ControllerByIdInfo.js";
 const log = getLogger("linter:xmlTemplate:Parser");
 
 export type Namespace = string;
@@ -133,7 +134,9 @@ export default class Parser {
 	#generator: AbstractGenerator;
 	#apiExtract: ApiExtract;
 
-	constructor(resourceName: string, apiExtract: ApiExtract, context: LinterContext) {
+	constructor(
+		resourceName: string, apiExtract: ApiExtract, context: LinterContext, controllerByIdInfo: ControllerByIdInfo
+	) {
 		const xmlDocumentKind = determineDocumentKind(resourceName);
 		if (xmlDocumentKind === null) {
 			throw new Error(`Unknown document type for resource ${resourceName}`);
@@ -141,8 +144,8 @@ export default class Parser {
 		this.#resourceName = resourceName;
 		this.#xmlDocumentKind = xmlDocumentKind;
 		this.#generator = xmlDocumentKind === DocumentKind.View ?
-			new ViewGenerator(resourceName) :
-			new FragmentGenerator(resourceName);
+			new ViewGenerator(resourceName, controllerByIdInfo) :
+			new FragmentGenerator(resourceName, controllerByIdInfo);
 
 		this.#apiExtract = apiExtract;
 		this.#context = context;

src/linter/xmlTemplate/generator/AbstractGenerator.ts
@@ -1,3 +1,4 @@
+import ControllerByIdInfo from "../ControllerByIdInfo.js";
 import {
 	ControlDeclaration, RequireExpression, Position,
 } from "../Parser.js";
@@ -17,10 +18,13 @@ export default abstract class AbstractGenerator {
 	_imports = new Set<ImportStatement>();
 	_variableNames = new Set<string>();
 	_body: Writer;
+	_idToModule = new Map<string, string>();
+	_controllerByIdInfo: ControllerByIdInfo;
 
-	constructor(filePath: string) {
+	constructor(filePath: string, controllerByIdInfo: ControllerByIdInfo) {
 		const fileName = path.basename(filePath, ".xml");
 		this._body = new Writer(fileName + ".js", fileName + ".xml");
+		this._controllerByIdInfo = controllerByIdInfo;
 	}
 
 	// eslint-disable-next-line @typescript-eslint/no-unused-vars
@@ -31,9 +35,11 @@ export default abstract class AbstractGenerator {
 	writeControl(controlDeclaration: ControlDeclaration) {
 		const importVariableName = this._getUniqueVariableName(controlDeclaration.name);
 
+		const moduleName = controlDeclaration.namespace.replaceAll(".", "/") + `/${controlDeclaration.name}`;
+
 		// Add import
 		this._imports.add({
-			moduleName: controlDeclaration.namespace.replaceAll(".", "/") + `/${controlDeclaration.name}`,
+			moduleName,
 			variableName: importVariableName,
 			start: controlDeclaration.start,
 			end: controlDeclaration.end,
@@ -46,6 +52,13 @@ export default abstract class AbstractGenerator {
 		this._body.writeln(`new ${importVariableName}({`, controlDeclaration.start, controlDeclaration.end);
 		// Write properties
 		controlDeclaration.properties.forEach((attribute) => {
+			// Add mapping of id to module name so that specific byId lookup for controllers can be generated.
+			// This information can only be added to the ControllerByIdInfo once the controllerName is known,
+			// which happens last as outer nodes are visited last.
+			if (attribute.name === "id") {
+				this._idToModule.set(attribute.value, moduleName);
+			}
+
 			// TODO: Determine attribute type based on metadata and parse/write value accordingly
 			this._body.write(`    `);
 			this._body.writeln(

src/linter/xmlTemplate/generator/ControllerByIdDtsGenerator.ts
@@ -1,0 +1,70 @@
+import ControllerByIdInfo, {IdModulesMap} from "../ControllerByIdInfo.js";
+
+export class ControllerByIdDtsGenerator {
+	// Maps module names to local names
+	private imports = new Map<string, string>();
+
+	generate(controllerByIdInfo: ControllerByIdInfo) {
+		const mappings = controllerByIdInfo.getMappings();
+		if (!mappings.size) {
+			return null;
+		}
+		this.imports = new Map<string, string>();
+		this.addImport("sap/ui/core/mvc/View"); // View is needed for interface ControllerView
+		this.addImport("sap/ui/core/Element"); // Element is needed for byId fallback signature
+		let out = "";
+		mappings.forEach((idToModules, controllerName) => {
+			out += this.generateModuleDeclaration(controllerName, idToModules);
+		});
+		return this.generateCollectedImports() + out;
+	}
+
+	private generateCollectedImports() {
+		return Array.from(this.imports.entries())
+			.map(([moduleName, localName]) => `import ${localName} from "${moduleName}";`)
+			.join("\n") + "\n";
+	}
+
+	private generateByIdMapping(idToModules: IdModulesMap) {
+		let out = "interface ByIdMapping {\n";
+		idToModules.forEach((modules, id) => {
+			const localNames = Array.from(modules).map((moduleName: string) => this.addImport(moduleName));
+			out += `\t\t"${id}": ${localNames.join(" | ")};\n`;
+		});
+		out += "\t}";
+		return out;
+	}
+
+	private generateModuleDeclaration(controllerName: string, idToModules: IdModulesMap) {
+		const moduleName = controllerName.replace(/\./g, "/") + ".controller";
+
+		return `
+declare module "${moduleName}" {
+	${this.generateByIdMapping(idToModules)}
+	type ByIdFunction = {
+		<T extends keyof ByIdMapping>(sId: T): ByIdMapping[T];
+		(sId: string): ${this.imports.get("sap/ui/core/Element")};
+	}
+	interface ControllerView extends ${this.imports.get("sap/ui/core/mvc/View")} {
+		byId: ByIdFunction;
+	}
+	export default interface Controller {
+		byId: ByIdFunction;
+		getView(): ControllerView;
+	}
+}
+`;
+	}
+
+	private addImport(moduleName: string) {
+		const localName = this.getLocalModuleName(moduleName);
+		if (!this.imports.has(moduleName)) {
+			this.imports.set(moduleName, localName);
+		}
+		return localName;
+	}
+
+	private getLocalModuleName(moduleName: string) {
+		return moduleName.replace(/[/.]/g, "_");
+	}
+}

commit 84be5c47792a7c205aa607ab746df843cb6333b7

fix: False-positive global findings in controllers
This solves an issue where the linter would falsely report access to
global variables in controller code when accessing the controller class
(e.g. by adding methods to the prototype).

The issue was caused by the introduction of type support for "byId" in
controllers and only appears when there is a view that references the
controller.

Caused by: https://github.com/SAP/ui5-linter/pull/423

src/linter/ui5Types/SourceFileLinter.ts
@@ -21,11 +21,11 @@ interface DeprecationInfo {
 }
 
 function isSourceFileOfUi5Type(sourceFile: ts.SourceFile) {
-	return /@openui5|@sapui5|@ui5/.test(sourceFile.fileName);
+	return /\/types\/(@openui5|@sapui5|@ui5\/linter\/overrides)\//.test(sourceFile.fileName);
 }
 
 function isSourceFileOfUi5OrThirdPartyType(sourceFile: ts.SourceFile) {
-	return /@openui5|@sapui5|@ui5|@types\/jquery/.test(sourceFile.fileName);
+	return isSourceFileOfUi5Type(sourceFile) || /\/types\/(@types\/jquery)\//.test(sourceFile.fileName);
 }
 
 function isSourceFileOfJquerySapType(sourceFile: ts.SourceFile) {

test/fixtures/linter/projects/com.ui5.troublesome.app/webapp/controller/Main.controller.js
@@ -1,7 +1,7 @@
 sap.ui.define(["./BaseController", "sap/m/MessageBox"], function (BaseController, MessageBox) {
 	"use strict";
 
-	return BaseController.extend("com.ui5.troublesome.app.controller.Main", {
+	const MainController = BaseController.extend("com.ui5.troublesome.app.controller.Main", {
 		sayHello: function () {
 			MessageBox.show("Hello World!");
 		},
@@ -32,4 +32,16 @@ sap.ui.define(["./BaseController", "sap/m/MessageBox"], function (BaseController
 			this.getView().byId("unknown").prop("foo", "bar");
 		}
 	});
+
+	// Note: Accessing the controller prototype should not cause a false positive for global variables,
+	// which was appearing in combination with the declaration merging for the byId type support.
+	MainController.prototype.doSomething = function () {
+
+		// byId type support should also work within methods attached to the prototype
+		this.byId("helloButton").attachTap(function() {
+			console.log("Tapped");
+		});
+	};
+
+	return MainController;
 });