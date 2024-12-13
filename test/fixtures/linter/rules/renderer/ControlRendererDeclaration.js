ca547878eee347c764bfda0d8ff5799df6110702
Yavor Ivanov <yavor.ivanov@sap.com>
2024-12-13 11:06:58 +0200

    test: Add test case with invalid render version

--- test/fixtures/linter/rules/renderer/ControlRendererDeclaration.js
+++ test/fixtures/linter/rules/renderer/ControlRendererDeclaration.js
@@ -1,3 +1,5 @@
+const { default: Renderer } = require("sap/ui/core/Renderer");
+
 sap.ui.define(["sap/ui/core/Control", "sap/m/Button"], function(Control, Button) {
 	const Example1 = Control.extend("sap.ui.demo.linter.controls.Example1", {
 		metadata: {},
@@ -21,5 +23,12 @@ sap.ui.define(["sap/ui/core/Control", "sap/m/Button"], function(Control, Button)
 	const Example5 = Button.extend("sap.ui.demo.linter.controls.Example5", {
 		metadata: {},
 		// Missing renderer declaration (deprecated)
+	});
+	const Example6 = Button.extend("sap.ui.demo.linter.controls.Example6", {
+		metadata: {},
+		renderer: {
+			apiVersion: 3, // Invalid API version
+			render: function(oRm, oControl) {}
+		}
 	});
 });
