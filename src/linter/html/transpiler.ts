2b8f4d71879e4ffd1aae21b6830a118419b3f4d1
Yavor Ivanov <yavor.ivanov@sap.com>
2024-12-17 13:34:53 +0200

    refactor: Align messages

--- src/linter/html/transpiler.ts
+++ src/linter/html/transpiler.ts
@@ -236,7 +236,7 @@ function checkBindingSyntaxAttr(attr: Attribute, report: HtmlReporter) {
 			name: attr.name.value,
 			messageDetails: "Only 'complex' is supported with UI5 2.x and automatically" +
 				" enforced by the UI5 runtime. Check all bindings whether they will be " +
-				"misinterpreted in 2.0 with binding syntax 'complex'.",
+				"misinterpreted in 2.x with binding syntax 'complex'.",
 		}, attr.name);
 	}
 }
