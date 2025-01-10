551b66c7651e44493b026b49e3f1479776dceb7a
Yavor Ivanov <yavor.ivanov@sap.com>
2025-01-10 19:09:36 +0200

    fix: Test

--- test/fixtures/linter/rules/NoDeprecatedApi/ModelDataTypes.js
+++ test/fixtures/linter/rules/NoDeprecatedApi/ModelDataTypes.js
@@ -1,13 +1,10 @@
-sap.ui.define([
-	"sap/m/Input",
-	(Input) => {
-		const input = new Input({
-			value: {
-				path: "/names/0/amount",
-				type: "sap.ui.model.type.Integer",
-				formatOptions: { minIntegerDigits: 3 },
-				constraints: { maximum: 1000 },
-			},
-		});
-	},
-]);
+sap.ui.define(["sap/m/Input"], (Input) => {
+	const input = new Input({
+		value: {
+			path: "/names/0/amount",
+			type: "sap.ui.model.type.Integer",
+			formatOptions: { minIntegerDigits: 3 },
+			constraints: { maximum: 1000 },
+		},
+	});
+});
