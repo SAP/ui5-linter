457cf957ce91c840426c0aa14924e50d71354503
Max Reichmann <max.reichmann@sap.com>
2025-01-14 15:49:05 +0100

    refactor(metadata): Improve generating and reading

--- src/utils/ApiExtract.ts
+++ src/utils/ApiExtract.ts
@@ -1,19 +1,18 @@
-/* eslint-disable @typescript-eslint/no-redundant-type-constituents */
 import {readFile} from "node:fs/promises";
 
 export type AllowedSymbolKind = "UI5Class" | "UI5Enum" | "UI5Interface" | "UI5Namespace" | "UI5Typedef" | "UI5Function";
-export type AllowedMetadataOptionType = "aggregations" | "associations" | "defaultAggregation" | "events" |
-	"methods" | "properties";
+export type AllowedMetadataOptionType = "aggregation" | "association" | "defaultAggregation" | "event" |
+	"method" | "property";
 /** @example "sap.m.Button" | "jQuery.sap" | "module:sap/base/Event" | "sap.ui.base.Object" */
 type UI5SymbolName = string;
 /** @example "sap.m.App": { "properties": ["backgroundColor", ...], ..., "extends": "sap.m.NavContainer" } */
 export interface MetadataRecord {
-	aggregations?: string[];
+	aggregation?: string[];
 	defaultAggregation?: string;
-	associations?: string[];
-	events?: string[];
-	methods?: string[];
-	properties?: string[];
+	association?: string[];
+	event?: string[];
+	method?: string[];
+	property?: string[];
 	extends?: UI5SymbolName;
 };
 
@@ -34,10 +33,15 @@ interface DeprecationInfo {
 export interface ApiExtract {
 	getAllOptionsByType(symbolName: string, option: AllowedMetadataOptionType, includeBorrowed?: boolean):
 		string[] | undefined;
-	getTypeByOption(symbolName: string, optionName: string, includeBorrowed?: boolean):
-		AllowedMetadataOptionType | string | undefined;
+	getTypeByOption(symbolName: string, optionName: string, checkBorrowed?: boolean):
+		AllowedMetadataOptionType | undefined;
 	getDefaultAggregation(className: string): string | undefined;
 	getDeprecationInfo(symbolName: string): DeprecationInfo | undefined;
+	isAggregation(symbolName: string, aggregationName: string, checkBorrowed?: boolean): boolean;
+	isAssociation(symbolName: string, associationName: string, checkBorrowed?: boolean): boolean;
+	isEvent(symbolName: string, eventName: string, checkBorrowed?: boolean): boolean;
+	isMethod(symbolName: string, methodName: string, checkBorrowed?: boolean): boolean;
+	isProperty(symbolName: string, propertyName: string, checkBorrowed?: boolean): boolean;
 }
 
 export class ApiExtractImpl implements ApiExtract {
@@ -49,15 +53,15 @@ export class ApiExtractImpl implements ApiExtract {
 
 	/**
 	 * Returns all options of a given option type in a UI5 symbol.
-	 * (including borrowed ones is optional)
-	 * @example getAllOptionsByType("sap.m.App", "properties", false)
+	 * (not including borrowed ones is optional)
+	 * @example getAllOptionsByType("sap.m.App", "property", false)
 	 * 	returns ["backgroundColor", "backgroundImage", "backgroundRepeat", *etc.*]
 	 * @example getAllOptionsByType("sap.m.App", "notExistingOptionType")
 	 * 	returns []
-	 * @example getAllOptionsByType("sap.xyz.notExistingSymbol", "properties")
+	 * @example getAllOptionsByType("sap.xyz.notExistingSymbol", "property")
 	 * 	returns undefined
 	 */
-	getAllOptionsByType(symbolName: string, optionType: AllowedMetadataOptionType, includeBorrowed = false):
+	getAllOptionsByType(symbolName: string, optionType: AllowedMetadataOptionType, includeBorrowed = true):
 		string[] | undefined {
 		const values: string[] = [];
 		let foundSymbolRecord = this.data.metadata[symbolName];
@@ -80,7 +84,7 @@ export class ApiExtractImpl implements ApiExtract {
 
 	/**
 	 * Returns the type of a given option in a UI5 symbol record.
-	 * (resolving borrowed ones is optional)
+	 * (not checking borrowed ones is optional)
 	 * @example getTypeByOption("sap.m.App", "backgroundColor")
 	 * 	returns "properties"
 	 * @example getTypeByOption("sap.m.App", "notExistingOption")
@@ -88,18 +92,19 @@ export class ApiExtractImpl implements ApiExtract {
 	 * @example getTypeByOption("sap.xyz.notExistingSymbol", "backgroundColor")
 	 * 	returns undefined
 	 */
-	getTypeByOption(symbolName: string, optionName: string, includeBorrowed = false):
-		AllowedMetadataOptionType | string | undefined {
+	getTypeByOption(symbolName: string, optionName: string, checkBorrowed = true):
+		AllowedMetadataOptionType | undefined {
 		let foundSymbolRecord = this.data.metadata[symbolName];
 		// Check own types and resolve borrowed types with "extends":
 		while (foundSymbolRecord) {
 			// Loop through all keys of the symbol record:
 			for (const [key, value] of Object.entries(foundSymbolRecord)) {
-				if ((Array.isArray(value) && value.includes(optionName)) || value === optionName) {
-					return key;
+				if (key !== "extends" &&
+					((Array.isArray(value) && value.includes(optionName)) || value === optionName)) {
+					return key as AllowedMetadataOptionType;
 				}
 			}
-			if (includeBorrowed && foundSymbolRecord.extends) {
+			if (checkBorrowed && foundSymbolRecord.extends) {
 				foundSymbolRecord = this.data.metadata[foundSymbolRecord.extends];
 			} else {
 				break;
@@ -107,6 +112,66 @@ export class ApiExtractImpl implements ApiExtract {
 		}
 		return undefined;
 	};
+
+	/**
+	 * Checks whether a given aggregation is part of a UI5 symbol.
+	 * @example isAggregation("sap.m.NavContainer", "pages")
+	 * 	returns true
+	 * @example isAggregation("sap.m.App", "pages", false) // pages is a borrowed aggregation of "sap.m.NavContainer"
+	 * 	returns false
+	 */
+	isAggregation(symbolName: string, aggregationName: string, checkBorrowed = true): boolean {
+		return this.getAllOptionsByType(symbolName, "aggregation",
+			checkBorrowed)?.includes(aggregationName) ?? false;
+	}
+
+	/**
+	 * Checks whether a given association is part of a UI5 symbol.
+	 * @example isAssociation("sap.m.NavContainer", "initialPage")
+	 * 	returns true
+	 * @example isAssociation("sap.m.App", "initialPage", false) // initialPage is borrowed from "sap.m.NavContainer"
+	 * 	returns false
+	 */
+	isAssociation(symbolName: string, associationName: string, checkBorrowed = true): boolean {
+		return this.getAllOptionsByType(symbolName, "association",
+			checkBorrowed)?.includes(associationName) ?? false;
+	}
+
+	/**
+	 * Checks whether a given property is part of a UI5 symbol.
+	 * @example isProperty("sap.m.App", "backgroundColor")
+	 * 	returns true
+	 * @example isProperty("sap.m.App", "pages") // pages is a borrowed default aggregation of "sap.m.NavContainer"
+	 * 	returns false
+	 */
+	isProperty(symbolName: string, propertyName: string, checkBorrowed = true): boolean {
+		return this.getAllOptionsByType(symbolName, "property",
+			checkBorrowed)?.includes(propertyName) ?? false;
+	}
+
+	/**
+	 * Checks whether a given event is part of a UI5 symbol.
+	 * @example isEvent("sap.m.NavContainer", "afterNavigate")
+	 * 	returns true
+	 * @example isEvent("sap.m.App", "afterNavigate", false) // afterNavigate is borrowed from "sap.m.NavContainer"
+	 * 	returns false
+	 */
+	isEvent(symbolName: string, eventName: string, checkBorrowed = true): boolean {
+		return this.getAllOptionsByType(symbolName, "event",
+			checkBorrowed)?.includes(eventName) ?? false;
+	}
+
+	/**
+	 * Checks whether a given method is part of a UI5 symbol.
+	 * @example isMethod("sap.m.App", "getBackgroundColor")
+	 * 	returns true
+	 * @example isMethod("sap.m.App", "addPage", false) // addPage is borrowed from "sap.m.NavContainer"
+	 * 	returns false
+	 */
+	isMethod(symbolName: string, methodName: string, checkBorrowed = true): boolean {
+		return this.getAllOptionsByType(symbolName, "method",
+			checkBorrowed)?.includes(methodName) ?? false;
+	}
 
 	/**
 	 * Returns the default aggregation of a given UI5 class.
--- test/lib/utils/ApiExtract.ts
+++ test/lib/utils/ApiExtract.ts
@@ -1,7 +1,10 @@
 import test from "ava";
-import {ApiExtractImpl} from "../../../src/utils/ApiExtract.js";
+import {
+	// ApiExtractImpl,
+	loadApiExtract} from "../../../src/utils/ApiExtract.js";
 
-const mockApiJSON = `
+/* Use the following mock JSON to test metadata reading independently from the actual api-extract.json file:
+const apiExtract = new ApiExtractImpl(JSON.parse(`
 {
 	"framework": {
 		"name": "SAPUI5",
@@ -9,13 +12,23 @@ const mockApiJSON = `
 	},
 	"metadata": {
 		"sap.m.App": {
-			"properties": ["backgroundColor", "backgroundImage"],
+			"property": ["backgroundColor", "backgroundImage"],
+			"event": ["orientationChange"],
+			"method": ["getBackgroundColor"],
 			"extends": "sap.m.NavContainer"
 		},
 		"sap.m.NavContainer": {
-			"properties": ["autoFocus"],
+			"property": ["autoFocus"],
+			"aggregation": ["pages"],
+			"association": ["initialPage"],
 			"defaultAggregation": "pages",
+			"event": ["afterNavigate"],
+			"method": ["addPage"],
 			"extends": "sap.ui.core.Control"
+		},
+		"sap.ui.core.Element": {
+			"aggregation": ["tooltip"],
+			"property": ["tooltip"]
 		}
 	},
 	"deprecations": {
@@ -23,41 +36,105 @@ const mockApiJSON = `
 			"sap.f.Avatar": "(since 1.73) Use the {@link sap.m.Avatar} instead."
 		}
 	}
-}`;
-
-const apiExtract = new ApiExtractImpl(JSON.parse(mockApiJSON));
+}`)); */
+const apiExtract = await loadApiExtract();
 
 test("Test getAllOptionsByType()", (t) => {
-	t.deepEqual(apiExtract.getAllOptionsByType("sap.m.App", "properties"),
-		["backgroundColor", "backgroundImage"],
+	const result = apiExtract.getAllOptionsByType("sap.m.App", "property", false);
+	const result2 = apiExtract.getAllOptionsByType("sap.m.App", "property");
+	const result3 = apiExtract.getAllOptionsByType("sap.m.App", "property", true);
+
+	t.truthy((Array.isArray(result) && result.length),
 		"All properties of 'sap.m.App' should be returned as an array");
-	t.deepEqual(apiExtract.getAllOptionsByType("sap.m.App", "properties", false),
-		["backgroundColor", "backgroundImage"],
-		"All properties of 'sap.m.App' should be returned as an array");
-	t.deepEqual(apiExtract.getAllOptionsByType("sap.m.App", "properties", true),
-		["backgroundColor", "backgroundImage", "autoFocus"],
+	t.truthy((Array.isArray(result) && Array.isArray(result2) && result2.length > result.length),
 		"All properties of 'sap.m.App' and its borrowed ones should be returned as an array");
-	t.deepEqual(apiExtract.getAllOptionsByType("sap.m.NavContainer", "properties", false), ["autoFocus"],
-		"All properties of 'sap.m.NavContainer' should be returned as an array");
-	t.is(apiExtract.getAllOptionsByType("sap.xyz.notExistingSymbol", "properties", false), undefined,
+	t.deepEqual(result2, result3, "All properties of 'sap.m.App' should be returned as an array");
+	t.deepEqual(apiExtract.getAllOptionsByType("sap.m.App", "defaultAggregation", false), [],
+		"If no options for the given type can be found, an empty array should be returned");
+	t.deepEqual(apiExtract.getAllOptionsByType("sap.m.App", "event", false), ["orientationChange"],
+		"'orientationChange' is the only event of 'sap.m.App' (but deprecated)");
+	t.is(apiExtract.getAllOptionsByType("sap.xyz.notExistingSymbol", "property", false), undefined,
 		"If the symbol can't be found, undefined should be returned");
-	t.is(apiExtract.getAllOptionsByType("sap.xyz.notExistingSymbol", "properties", true), undefined,
+	t.is(apiExtract.getAllOptionsByType("sap.xyz.notExistingSymbol", "property", true), undefined,
 		"If the symbol can't be found, undefined should be returned");
 });
 
-test("Test getTypeByOption()", (t) => {
-	t.is(apiExtract.getTypeByOption("sap.m.App", "backgroundColor"), "properties",
+test("Test getting option types by option name", (t) => {
+	t.is(apiExtract.isAggregation("sap.m.App", "pages"), true,
+		"'pages' is a borrowed aggregation of 'sap.m.NavContainer'");
+	t.is(apiExtract.isAggregation("sap.m.NavContainer", "pages"), true,
+		"'pages' is an aggregation of 'sap.m.NavContainer'");
+	t.is(apiExtract.isAggregation("sap.m.NavContainer", "pages", false), true,
+		"'pages' is an aggregation of 'sap.m.NavContainer'");
+	t.is(apiExtract.isAggregation("sap.m.App", "backgroundColor", false), false,
+		"'backgroundColor' is a property and not an aggregation of 'sap.m.App'");
+	t.is(apiExtract.isAggregation("sap.xyz.notExistingSymbol", "backgroundColor"), false,
+		"If the symbol can't be found, false should be returned");
+
+	t.is(apiExtract.isAssociation("sap.m.App", "initialPage"), true,
+		"'initialPage' is a borrowed association of 'sap.m.NavContainer'");
+	t.is(apiExtract.isAssociation("sap.m.NavContainer", "initialPage"), true,
+		"'initialPage' is an association of 'sap.m.NavContainer'");
+	t.is(apiExtract.isAssociation("sap.m.NavContainer", "initialPage", false), true,
+		"'initialPage' is an aggregation of 'sap.m.NavContainer'");
+	t.is(apiExtract.isAssociation("sap.m.App", "backgroundColor", false), false,
+		"'backgroundColor' is a property and not an association of 'sap.m.App'");
+	t.is(apiExtract.isAssociation("sap.xyz.notExistingSymbol", "initialPage"), false,
+		"If the symbol can't be found, false should be returned");
+
+	t.is(apiExtract.isEvent("sap.m.App", "afterNavigate"), true,
+		"'afterNavigate' is a borrowed event of 'sap.m.NavContainer'");
+	t.is(apiExtract.isEvent("sap.m.NavContainer", "afterNavigate"), true,
+		"'afterNavigate' is an event of 'sap.m.NavContainer'");
+	t.is(apiExtract.isEvent("sap.m.NavContainer", "afterNavigate", false), true,
+		"'afterNavigate' is an event of 'sap.m.NavContainer'");
+	t.is(apiExtract.isEvent("sap.m.App", "backgroundColor", false), false,
+		"'backgroundColor' is a property and not an event of 'sap.m.App'");
+	t.is(apiExtract.isEvent("sap.xyz.notExistingSymbol", "afterNavigate"), false,
+		"If the symbol can't be found, false should be returned");
+
+	/* Disabled as "methods" not being required:
+	t.is(apiExtract.isMethod("sap.m.App", "getBackgroundColor"), true,
+		"'getBackgroundColor' is a method of 'sap.m.App'");
+	t.is(apiExtract.isMethod("sap.m.NavContainer", "addPage"), true,
+		"'addPage' is a borrowed method of 'sap.m.NavContainer'");
+	t.is(apiExtract.isMethod("sap.m.NavContainer", "addPage", false), true,
+		"'addPage' is a method of 'sap.m.NavContainer'");
+	t.is(apiExtract.isMethod("sap.m.App", "backgroundColor", false), false,
+		"'backgroundColor' is a property and not a method of 'sap.m.App'");
+	t.is(apiExtract.isMethod("sap.xyz.notExistingSymbol", "getBackgroundColor"), false,
+		"If the symbol can't be found, false should be returned");
+	*/
+
+	t.is(apiExtract.isProperty("sap.m.App", "backgroundColor"), true,
 		"'backgroundColor' is a property of 'sap.m.App'");
-	t.is(apiExtract.getTypeByOption("sap.m.App", "backgroundColor", false), "properties",
+	t.is(apiExtract.isProperty("sap.m.App", "backgroundColor", true), true,
 		"'backgroundColor' is a property of 'sap.m.App'");
-	t.is(apiExtract.getTypeByOption("sap.m.App", "pages", true), "defaultAggregation",
+	t.is(apiExtract.isProperty("sap.m.App", "autoFocus", true), true,
+		"'autoFocus' is a property of 'sap.m.App'");
+	t.is(apiExtract.isProperty("sap.m.App", "backgroundColor", false), true,
+		"'backgroundColor' is a property of 'sap.m.App'");
+	t.is(apiExtract.isProperty("sap.m.App", "pages", false), false,
+		"'pages' is an aggregation and not a property of 'sap.m.App'");
+	t.is(apiExtract.isProperty("sap.xyz.notExistingSymbol", "backgroundColor"), false,
+		"If the symbol can't be found, false should be returned");
+
+	t.is(apiExtract.getTypeByOption("sap.m.App", "pages"), "aggregation",
 		"'pages' is the default aggregation of 'sap.m.App'");
-	t.is(apiExtract.getTypeByOption("sap.m.App", "notExistingOptionType", true), undefined,
+	t.is(apiExtract.getTypeByOption("sap.m.App", "pages", false), undefined,
+		`'pages' is a borrowed default aggregation of 'sap.m.NavContainer',
+hence it should not be found in 'sap.m.App'`);
+	t.is(apiExtract.getTypeByOption("sap.m.App", "notExistingOptionType"), undefined,
 		"If the option type can't be found, undefined should be returned");
-	t.is(apiExtract.getTypeByOption("sap.ui.base.Object", "backgroundColor", true), undefined,
+	t.is(apiExtract.getTypeByOption("sap.ui.base.Object", "backgroundColor"), undefined,
 		"'sap.ui.base.Object' does not have any options, hence undefined should be returned");
-	t.is(apiExtract.getTypeByOption("sap.xyz.notExistingSymbol", "backgroundColor", true), undefined,
+	t.is(apiExtract.getTypeByOption("sap.xyz.notExistingSymbol", "backgroundColor"), undefined,
 		"If the symbol can't be found, undefined should be returned");
+
+	// Test aggregation exception:
+	t.true(apiExtract.isAggregation("sap.ui.core.Element", "tooltip") &&
+		apiExtract.isProperty("sap.ui.core.Element", "tooltip"),
+	"'tooltip' is an aggregation and a property of 'sap.ui.core.Element'");
 });
 
 test("Test getDefaultAggregation()", (t) => {
