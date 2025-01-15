d6aafd27a59c7da851ffbec17aac7f0bd0747d94
Yavor Ivanov <yavor.ivanov@sap.com>
2025-01-15 12:27:55 +0200

    test: Add more test cases

---
+++ test/fixtures/linter/rules/NoDeprecatedApi/ModelDataTypes_NewStringNegative.js
@@ -0,0 +1,10 @@
+sap.ui.define(
+	["sap/m/Input", "sap/ui/model/type/Integer"],
+	(Input, Integer) => {
+		"use strict";
+
+		const input = new Input({
+			value: "{ path: '/names/0/amount', type: 'sap.ui.model.type.Integer' }",
+		});
+	}
+);
