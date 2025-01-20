1c90c3f3f5cae62231615d8a25a32f0a5f4d83f8
Yavor Ivanov <yavor.ivanov@sap.com>
2025-01-20 15:42:07 +0200

    refactor: Stash information about the XML transpilation

--- src/linter/ui5Types/SourceFileLinter.ts
+++ src/linter/ui5Types/SourceFileLinter.ts
@@ -1262,9 +1262,8 @@ export default class SourceFileLinter {
 					prop.initializer.text.startsWith("{") && prop.initializer.text.endsWith("}") &&
 					ts.isNewExpression(node) &&
 					this.#isPropertyBindingType(node, prop.name.getText())) {
-					const nodeSourceMap = this.sourceMaps?.get(this.resourcePath);
-					const traceMap = nodeSourceMap ? new TraceMap(nodeSourceMap) : undefined;
-					const originalFilename = traceMap?.sources[0] ?? traceMap?.file;
+					const originalFilename = Array.from(
+						this.context.getMetadata(this.resourcePath)?.xmlCompiledResources ?? [])[0];
 					let resourcePath = this.resourcePath;
 					if (originalFilename) {
 						if ([".view.xml", ".fragment.xml"].some((ending) => originalFilename.endsWith(ending))) {
@@ -1284,6 +1283,8 @@ export default class SourceFileLinter {
 							} as RequireDeclaration;
 						});
 
+					const nodeSourceMap = this.sourceMaps?.get(this.resourcePath);
+					const traceMap = nodeSourceMap ? new TraceMap(nodeSourceMap) : undefined;
 					const {start: nodePos} = getPositionsForNode({
 						node: prop,
 						sourceFile: this.sourceFile,
