f65a92db193911345805e427aef46894912ea02f
Yavor Ivanov <yavor.ivanov@sap.com>
2025-01-20 14:04:01 +0200

    refactor: DotLibrary migration

--- src/linter/dotLibrary/DotLibraryLinter.ts
+++ src/linter/dotLibrary/DotLibraryLinter.ts
@@ -1,7 +1,7 @@
 import LinterContext from "../LinterContext.js";
 import {deprecatedLibraries} from "../../utils/deprecations.js";
 import {SaxEventType, Tag as SaxTag} from "sax-wasm";
-import {parseXML} from "../../utils/xmlParser.js";
+import {parseXML, SaxParserToJSON} from "../../utils/xmlParser.js";
 import {ReadStream} from "node:fs";
 import {MESSAGE} from "../messages.js";
 
@@ -26,7 +26,7 @@ export default class DotLibraryLinter {
 		}
 	}
 
-	async #parseDotLibrary(contentStream: ReadStream): Promise<SaxTag[]> {
+	async #parseDotLibrary(contentStream: ReadStream): Promise<SaxParserToJSON[]> {
 		const libs = new Set();
 		const tagsStack: string[] = [];
 		const libNamePath = ["library", "dependencies", "dependency"];
@@ -35,27 +35,28 @@ export default class DotLibraryLinter {
 				return;
 			}
 
-			if (event === SaxEventType.OpenTag && !tag.selfClosing) {
-				tagsStack.push(tag.value);
-			} else if (event === SaxEventType.CloseTag && !tag.selfClosing) {
+			const serializedTag = tag.toJSON();
+			if (event === SaxEventType.OpenTag && !serializedTag.selfClosing) {
+				tagsStack.push(serializedTag.name);
+			} else if (event === SaxEventType.CloseTag && !serializedTag.selfClosing) {
 				tagsStack.pop();
 			}
 
 			if (event === SaxEventType.CloseTag &&
-				tag.value === "libraryName") {
+				serializedTag.name === "libraryName") {
 				const isMatchingPath = libNamePath.length === tagsStack.length &&
 					libNamePath.every((lib, index) => lib === tagsStack[index]);
 
 				if (isMatchingPath) {
-					libs.add(tag);
+					libs.add(serializedTag);
 				}
 			}
 		});
 
-		return Array.from(libs) as SaxTag[];
+		return Array.from(libs) as SaxParserToJSON[];
 	}
 
-	#analyzeDeprecatedLibs(libs: SaxTag[]) {
+	#analyzeDeprecatedLibs(libs: SaxParserToJSON[]) {
 		// Check for deprecated libraries
 		libs.forEach((lib) => {
 			// textNodes is always an array, but it might be empty
