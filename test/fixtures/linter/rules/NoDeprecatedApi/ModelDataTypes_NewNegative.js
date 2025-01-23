81e694062251f21f44c7d6974129267216ab0952
Yavor Ivanov <yavor.ivanov@sap.com>
2025-01-23 16:03:06 +0200

    test: Add negative fixture

--- test/fixtures/linter/rules/NoDeprecatedApi/ModelDataTypes_NewNegative.js
+++ test/fixtures/linter/rules/NoDeprecatedApi/ModelDataTypes_NewNegative.js
@@ -19,5 +19,14 @@ sap.ui.define(
 				constraints: { maximum: 1000 },
 			},
 		});
+		
+		input.applySettings({
+			value: {
+				path: "/names/0/amount",
+				type: "sap.ui.model.type.Integer",
+				formatOptions: { minIntegerDigits: 3 },
+				constraints: { maximum: 1000 },
+			},
+		});
 	}
 );
