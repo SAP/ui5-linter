46ed7aed463d80ecc8e99afe282916fdec7e9266
Yavor Ivanov <yavor.ivanov@sap.com>
2025-01-30 11:15:58 +0200

    fix: CodeQL scan

--- test/fixtures/linter/projects/com.ui5.troublesome.app/ui5lint-custom-broken.config.cjs
+++ test/fixtures/linter/projects/com.ui5.troublesome.app/ui5lint-custom-broken.config.cjs
@@ -1,4 +1,7 @@
-module.exports = {
-	ignores: [
+// CodeQL code scan complains about this file being broken.
+// This is a case we test for the config manager. So, wrapping it in a JSON.parse to avoid the error there,
+// but keep the test case valid.
+module.exports = JSON.parse(`{
+	"ignores": [
 		"test/**/*",
-		"!test/sap/m/visual/Wizard.spe
+		"!test/sap/m/visual/Wizard.spe`);
