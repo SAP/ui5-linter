e55e134fd71a08fdb7eedb90da21fc84f997c977
Yavor Ivanov <yavor.ivanov@sap.com>
2025-02-05 12:22:03 +0200

    test: Update snapshots

--- test/fixtures/transpiler/xml-in-js/SapUIFragment.js
+++ test/fixtures/transpiler/xml-in-js/SapUIFragment.js
@@ -1,10 +1,18 @@
 sap.ui.require([], () => {
 	const fragment = sap.ui.fragment({
 		type: "XML",
-		fragmentContent: `<Button tap=".sayHello">`,
+		fragmentContent: `<core:FragmentDefinition
+		xmlns="sap.m"
+		xmlns:core="sap.ui.core">
+			<Button tap=".sayHello">
+		</core:FragmentDefinition>`,
 	});
 	
 	const fragment2 = sap.ui.xmlfragment({
-		fragmentContent: `<Button tap=".sayHello">`,
+		fragmentContent: `<core:FragmentDefinition
+		xmlns="sap.m"
+		xmlns:core="sap.ui.core">
+			<Button tap=".sayHello">
+		</core:FragmentDefinition>`,
 	});
 });
