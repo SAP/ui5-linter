bda9479e042f9947316e0ea4729595df94eac5a5
Yavor Ivanov <yavor.ivanov@sap.com>
2025-02-07 11:14:44 +0200

    refactor: Detect precisely the imports (in progress)

--- src/linter/xmlInJs/transpile.ts
+++ src/linter/xmlInJs/transpile.ts
@@ -1,5 +1,58 @@
 import ts from "typescript";
 import {DecodedSourceMap, SourceMapInput, TraceMap, decodedMap, SourceMapSegment} from "@jridgewell/trace-mapping";
+import {taskStart} from "../../utils/perf.js";
+
+// Function to extract imports
+function getRawUI5AmdImports(sourceFile: ts.SourceFile) {
+	const imports: {module: string; specifiers: string[]}[] = [];
+
+
+	return imports;
+}
+
+function zzzz(sourceFile: ts.SourceFile, node: ts.Node, methodName: string) {
+	const mainNode = node.parent.parent;
+
+	if (!ts.isCallExpression(mainNode)) {
+		return false;
+	}
+
+	const nameChunks = [];
+	let nameTraversalNode = mainNode.expression;
+	while (ts.isPropertyAccessExpression(nameTraversalNode)) {
+		nameChunks.unshift(nameTraversalNode.name.text);
+		nameTraversalNode = nameTraversalNode.expression;
+	}
+
+	let potentialGlobalName;
+	if (ts.isIdentifier(nameTraversalNode)) {
+		potentialGlobalName = nameTraversalNode.text;
+		nameChunks.unshift(potentialGlobalName);
+	}
+
+	// TODO: Identify more correctly the prop:
+	// "definition": ["create", "load", "sap.ui.view"],
+	// "viewContent": ["sap.ui.xmlview", "sap.ui.view"],
+	// "fragmentContent": ["sap.ui.fragment", "sap.ui.xmlfragment"],
+
+	if (!potentialGlobalName) {
+		return false;
+	}
+
+	const name = nameChunks.join(".");
+	if ((methodName === "definition" && ["sap.ui.view"].includes(name)) ||
+		(methodName === "viewContent" && ["sap.ui.xmlview", "sap.ui.view"].includes(name)) ||
+		(methodName === "fragmentContent" && ["sap.ui.fragment", "sap.ui.xmlfragment"].includes(name))) {
+		return true;
+	} else if (methodName === "definition" && ["create", "load"].includes(nameChunks[nameChunks.length - 1])) {
+		const imports = getRawUI5AmdImports(sourceFile);
+
+		// return
+	}
+
+
+	return false;
+}
 
 /**
  * Gets the original JS file and looks for specific properties that might eventually
@@ -8,6 +61,7 @@ import {DecodedSourceMap, SourceMapInput, TraceMap, decodedMap, SourceMapSegment
 export function extractXMLFromJs(filePath: string, fileContent: string) {
 	const viewProps = ["definition", "fragmentContent", "viewContent"];
 	if (viewProps.some((prop) => fileContent.includes(`${prop}:`))) {
+		const taskEnd = taskStart("Extract XML snippet from", filePath, true);
 		const sourceFile = ts.createSourceFile(filePath, fileContent, ts.ScriptTarget.Latest, true);
 
 		const xmlContents: {xml: string; pos: ts.LineAndCharacter}[] = [];
@@ -18,10 +72,6 @@ export function extractXMLFromJs(filePath: string, fileContent: string) {
 						ts.isIdentifier(prop.name) &&
 						ts.isStringLiteralLike(prop.initializer) &&
 						viewProps.includes(prop.name.text)) {
-						// TODO: Identify more correctly the prop:
-						// "definition": ["create", "load", "sap.ui.view"],
-						// "viewContent": ["sap.ui.xmlview", "sap.ui.view"],
-						// "fragmentContent": ["sap.ui.fragment", "sap.ui.xmlfragment"],
 						xmlContents.push({
 							xml: prop.initializer.text,
 							pos: sourceFile.getLineAndCharacterOfPosition(prop.initializer.pos),
@@ -33,6 +83,8 @@ export function extractXMLFromJs(filePath: string, fileContent: string) {
 		}
 
 		findProperty(sourceFile);
+		taskEnd();
+
 		return xmlContents.map((xmlContent, idx) => ({
 			originalPath: filePath,
 			path: `${sourceFile.fileName.replace(/(\.js|\.ts)$/, "")}.inline-${idx + 1}.view.xml`,
