bf6777c03a2f8d89d264b56b43bd31d360c007fd
Yavor Ivanov <yavor.ivanov@sap.com>
2025-02-10 14:32:41 +0200

    refactor: Cleanups

--- test/lib/linter/xmlInJs/transpiler.ts
+++
@@ -1,8 +0,0 @@
-import path from "node:path";
-import {fileURLToPath} from "node:url";
-import {createTestsForFixtures} from "./_helper.js";
-
-const __dirname = path.dirname(fileURLToPath(import.meta.url));
-const fixtures = path.join(__dirname, "..", "..", "..", "fixtures", "transpiler", "xml-in-js");
-
-createTestsForFixtures(fixtures);
