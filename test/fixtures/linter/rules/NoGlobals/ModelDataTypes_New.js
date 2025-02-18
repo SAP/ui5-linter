0e410dbd34d168bed3a59b45f6e1d247dfa804e8
Matthias Osswald <mat.osswald@sap.com>
2025-02-18 08:55:06 +0100

    fix: Check bindings of all ManagedObject subclasses
    Follow-up of https://github.com/SAP/ui5-linter/pull/513

--- test/fixtures/linter/rules/NoGlobals/ModelDataTypes_New.js
+++ test/fixtures/linter/rules/NoGlobals/ModelDataTypes_New.js
@@ -1,4 +1,4 @@
-sap.ui.define(["sap/m/Input"], (Input) => {
+sap.ui.define(["sap/m/Input", "sap/ui/table/RowSettings"], (Input, RowSettings) => {
 	const input = new Input({
 		value: {
 			path: "/names/0/amount",
@@ -7,7 +7,7 @@ sap.ui.define(["sap/m/Input"], (Input) => {
 			constraints: { maximum: 1000 },
 		},
 	});
-	
+
 	input.applySettings({
 		value: {
 			path: "/names/0/amount",
@@ -15,5 +15,20 @@ sap.ui.define(["sap/m/Input"], (Input) => {
 			formatOptions: { minIntegerDigits: 3 },
 			constraints: { maximum: 1000 },
 		},
+	});
+
+	// Note: RowSettings is an element, not a control (bindings are supported for all ManagedObject sub-classes)
+	const rowSettings = new RowSettings({
+		highlight: {
+			path: "/names/0/amount",
+			type: "sap.ui.model.type.Integer"
+		}
+	});
+
+	rowSettings.applySettings({
+		highlight: {
+			path: "/names/0/amount",
+			type: "sap.ui.model.type.Integer"
+		}
 	});
 });
--- test/fixtures/linter/rules/NoGlobals/ModelDataTypes_NewString.js
+++ test/fixtures/linter/rules/NoGlobals/ModelDataTypes_NewString.js
@@ -1,9 +1,18 @@
-sap.ui.define(["sap/m/Input"], (Input) => {
+sap.ui.define(["sap/m/Input", "sap/ui/table/RowSettings"], (Input, RowSettings) => {
 	const input = new Input({
 		value: "{ path: '/names/0/amount', type: 'sap.ui.model.type.Integer' }",
 	});
-	
+
 	input.applySettings({
 		value: "{ path: '/names/0/amount', type: 'sap.ui.model.type.Integer' }",
+	});
+
+	// Note: RowSettings is an element, not a control (bindings are supported for all ManagedObject sub-classes)
+	const rowSettings = new RowSettings({
+		highlight: "{ path: '/names/0/amount', type: 'sap.ui.model.type.Integer' }",
+	});
+
+	rowSettings.applySettings({
+		highlight: "{ path: '/names/0/amount', type: 'sap.ui.model.type.Integer' }",
 	});
 });
