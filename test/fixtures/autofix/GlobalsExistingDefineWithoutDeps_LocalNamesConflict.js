33bc7ea33aa0733f1c5b894854c01ec2e938cef8
Matthias Osswald <mat.osswald@sap.com>
2025-03-11 10:34:32 +0100

    test: Add local name conflict test fixture

---
+++ test/fixtures/autofix/GlobalsExistingDefineWithoutDeps_LocalNamesConflict.js
@@ -0,0 +1,8 @@
+sap.ui.define(function() {
+	// The following local names are the preferred unique names for sap/m/Button.
+	// This test ensures that a new unique name is generated.
+	const Button = new sap.m.Button();
+	const MButton = new sap.m.Button();
+	const SapMButton = new sap.m.Button();
+	const SapMButton0 = new sap.m.Button();
+});
