173192919dcfb692840c514e35383a1900169b4f
Yavor Ivanov <yavor.ivanov@sap.com>
2025-04-14 17:41:59 +0300

    docs: Update comments

--- src/linter/ui5Types/FixHintsGenerator.ts
+++ src/linter/ui5Types/FixHintsGenerator.ts
@@ -143,7 +143,7 @@ $identifier_1.forEach(({protocol, host, port, path}) => $moduleIdentifier.add(pr
 	}],
 	["charToUpperCase", {
 		moduleName: "sap/base/strings/capitalize",
-		exportCodeToBeUsed: "$moduleIdentifier($1, $2)", // Needs special handling for edge cases
+		exportCodeToBeUsed: "$moduleIdentifier($1, $2)", // Has special handling for edge cases
 	}],
 	["escapeRegExp", {
 		moduleName: "sap/base/strings/escapeRegExp",
