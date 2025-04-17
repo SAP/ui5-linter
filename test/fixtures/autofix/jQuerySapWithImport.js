aa0ee8d0fdd2552be4adf9ec194d64c613f45cf0
Yavor Ivanov <yavor.ivanov@sap.com>
2025-04-17 12:47:28 +0300

    fix: Adjust jQuery.fn.control

--- test/fixtures/autofix/jQuerySapWithImport.js
+++ test/fixtures/autofix/jQuerySapWithImport.js
@@ -4,4 +4,6 @@ sap.ui.define([], function () {
 	
 	// https://github.com/SAP/ui5-linter/issues/578
 	var myFancyControl = jQuery(".fancyContainer").control();
+	var myFancyControl2 = jQuery(".fancyContainer").control(0);
+	var myFancyControl2 = jQuery(".fancyContainer").control(0, true);
 });
