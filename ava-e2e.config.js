261599e971d2fe251fbe216dda46200662188cd9
Matthias Osswald <mat.osswald@sap.com>
2025-05-08 12:26:47 +0200

    test(e2e): Add runtime tests for autofix
    The tests are executed in the browser before and after running autofix
    to ensure that the applied changes are compatible and do not introduce
    regressions.
    
    Also adding new licensee exceptions for packages licensed under MIT
    (transitive dependencies of ui5-test-runner).

--- ava-e2e.config.js
+++ ava-e2e.config.js
@@ -1,5 +1,6 @@
 import defaultAvaConfig from "./ava.config.js";
 
 defaultAvaConfig.files = ["test/e2e/**/*.ts"];
+defaultAvaConfig.timeout = "60s"; // Increased timeout for slower CI environments
 
 export default defaultAvaConfig;
