cb8606e14d060ba021e0aa813719c6946d997efc
Matthias Osswald <mat.osswald@sap.com>
2025-05-13 12:37:43 +0200

    test: Check result in runtime e2e test

--- test/e2e/runtime.ts
+++ test/e2e/runtime.ts
@@ -13,6 +13,33 @@ const RUNTIME_DIR = fileURLToPath(new URL("../../test/fixtures/e2e/runtime", imp
 const TMP_RUNTIME_DIR = fileURLToPath(new URL("../../test/tmp/e2e/runtime", import.meta.url));
 const TMP_RUNTIME_DIR_AUTOFIX = fileURLToPath(new URL("../../test/tmp/e2e/runtime-autofix", import.meta.url));
 
+async function execUi5Lint(args: string[], cwd: string) {
+	let stdout = "", stderr = "", exitCode = 0;
+	try {
+		const result = await execFile("node", [UI5LINT_BIN, "--format", "json", ...args], {cwd});
+		stderr = result.stderr;
+		stdout = result.stdout;
+	} catch (error) {
+		if (error instanceof Error) {
+			if ("stdout" in error && typeof error.stdout === "string") {
+				stdout = error.stdout;
+			}
+			if ("stderr" in error && typeof error.stderr === "string") {
+				stderr = error.stderr;
+			}
+			if ("code" in error && typeof error.code === "number") {
+				exitCode = error.code;
+			}
+		} else {
+			throw error;
+		}
+	}
+	if (stdout) {
+		stdout = JSON.parse(stdout);
+	}
+	return {stdout, stderr, exitCode};
+}
+
 async function copyDir(src: string, dest: string) {
 	await fs.mkdir(dest, {recursive: true});
 	const entries = await fs.readdir(src, {withFileTypes: true});
@@ -42,9 +69,10 @@ test.serial("Run tests after autofix", async (t) => {
 	await fs.rm(TMP_RUNTIME_DIR_AUTOFIX, {recursive: true, force: true});
 	await copyDir(RUNTIME_DIR, TMP_RUNTIME_DIR_AUTOFIX);
 
-	await t.notThrowsAsync(execFile("node", [UI5LINT_BIN, "--fix"], {
-		cwd: TMP_RUNTIME_DIR_AUTOFIX,
-	}));
+	const {stdout, stderr, exitCode} = await execUi5Lint(["--fix"], TMP_RUNTIME_DIR_AUTOFIX);
+	t.snapshot(stderr, "stderr");
+	t.snapshot(stdout, "stdout");
+	t.snapshot(exitCode, "exitCode");
 
 	await t.notThrowsAsync(execFile("npm", ["test"], {
 		cwd: TMP_RUNTIME_DIR_AUTOFIX,
