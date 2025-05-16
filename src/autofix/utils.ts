18732fa091c86085e2c13eabdc0f79cd2dbd15e0
Yavor Ivanov <yavor.ivanov@sap.com>
2025-05-16 09:34:26 +0300

    refactor: Extract namespace resolver into util

--- src/autofix/utils.ts
+++ src/autofix/utils.ts
@@ -1,5 +1,79 @@
 import ts from "typescript";
-import Fix from "../linter/ui5Types/fix/Fix.js";
+import {getPropertyNameText} from "../linter/ui5Types/utils/utils.js";
+
+export function resolveNamespace(node: ts.AccessExpression | ts.CallExpression): string | undefined {
+	const firstPart = node.expression;
+	const parts: string[] = [];
+	if (!ts.isIdentifier(firstPart)) {
+		if (!ts.isCallExpression(firstPart)) {
+			return undefined;
+		}
+	} else {
+		if (firstPart.text !== "window" && firstPart.text !== "globalThis" && firstPart.text !== "self") {
+			parts.push(firstPart.text);
+		}
+	}
+
+	let scanNode: ts.Node = node;
+	while (ts.isPropertyAccessExpression(scanNode)) {
+		if (!ts.isIdentifier(scanNode.name)) {
+			throw new Error(
+				`Unexpected PropertyAccessExpression node: Expected name to be identifier but got ` +
+				ts.SyntaxKind[scanNode.name.kind]);
+		}
+		parts.push(scanNode.name.text);
+		scanNode = scanNode.parent;
+	}
+
+	return parts.join(".");
+}
+
+export function findGreatestAccessExpression(node: ts.Identifier, matchPropertyAccess?: string):
+	ts.Identifier | ts.PropertyAccessExpression | ts.ElementAccessExpression {
+	type Candidate = ts.Identifier | ts.PropertyAccessExpression | ts.ElementAccessExpression;
+	let scanNode: Candidate = node;
+	let propertyAccessChain: string[] = [];
+	if (matchPropertyAccess) {
+		propertyAccessChain = matchPropertyAccess.split(".");
+		if (node.text !== "window" && node.text !== "globalThis" && node.text !== "self") {
+			const firstPropAccess = propertyAccessChain.shift();
+			if (node.text !== firstPropAccess) {
+				throw new Error(`Expected node to be ${firstPropAccess} but got ${node.getText()}`);
+			}
+			if (!propertyAccessChain.length) {
+				return scanNode;
+			}
+		}
+	}
+
+	while (ts.isPropertyAccessExpression(scanNode.parent) || ts.isElementAccessExpression(scanNode.parent)) {
+		scanNode = scanNode.parent;
+		if (matchPropertyAccess) {
+			const nextPropertyAccess = propertyAccessChain.shift();
+
+			let propName;
+			if (ts.isPropertyAccessExpression(scanNode)) {
+				propName = getPropertyNameText(scanNode.name);
+			} else {
+				if (
+					ts.isStringLiteralLike(scanNode.argumentExpression) ||
+					ts.isNumericLiteral(scanNode.argumentExpression)
+				) {
+					propName = scanNode.argumentExpression.text;
+				} else {
+					propName = scanNode.argumentExpression.getText();
+				}
+			}
+			if (propName !== nextPropertyAccess) {
+				throw new Error(`Expected node to be ${nextPropertyAccess} but got ${propName}`);
+			}
+			if (!propertyAccessChain.length) {
+				return scanNode;
+			}
+		}
+	}
+	return scanNode;
+}
 
 export function matchPropertyAccessExpression(node: ts.PropertyAccessExpression, match: string): boolean {
 	const propAccessChain: string[] = [];
