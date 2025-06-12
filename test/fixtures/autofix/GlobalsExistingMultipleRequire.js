2202a6f082af173bbc20c0e090fdecc518119e3d
Merlin Beutlberger <m.beutlberger@sap.com>
2025-06-12 11:11:04 +0200

    refactor(autofix): Handle multiple require statements on the same level
    If there is no single "root" sap.ui.define statement, there can be
    multiple root nodes.

---
+++ test/fixtures/autofix/GlobalsExistingMultipleRequire.js
@@ -0,0 +1,45 @@
+(() => {
+	sap.ui.require([
+		"sap/m/Button",
+		"sap/m/Avatar",
+		'sap/m/ComboBox',
+	], function(
+		ButtonRenamed,
+		Avatar
+	) {
+		const avatarDOM = jQuery("#container-todo---app--avatar-profile");
+		const list = sap.ui.getCore().byId("container-todo---app--todoList");
+		sap.m.BackgroundDesign.Solid
+		const button = new sap.m.Button({
+			text: "Hello"
+		});
+		const button2 = new ButtonRenamed({
+			text: "Hello"
+		});
+
+		const avatar = new sap.m.Avatar();
+	});
+
+	sap.ui.require(["sap/m/Dialog", "sap/m/MessageToast", "sap/f/library"], function(Dialog, MessageToast, fLib) {
+		sap.f.AvatarType.Icon;
+		fLib.AvatarType.Image;
+	});
+
+	// Require with no callback
+	sap.ui.require(["sap/m/Dialog"]);
+
+	sap.ui.require([], () => {
+		const button3 = new window.sap.m.Button({
+			text: "Hello"
+		});
+		const fileUploader = new sap.ui.unified.FileUploader({
+			valueState: sap.ui.core.ValueState.Success
+		});
+		const core = sap.ui.core;
+		const fileUploader2 = new sap.ui.unified.FileUploader({
+			valueState: core.ValueState.Success
+		});
+		sap.ui.view("myView");
+		sap.m.URLHelper.triggerSms();
+	});
+});
