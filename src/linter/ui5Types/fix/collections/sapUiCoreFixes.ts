e649cd3a1a8c09fa712a88efa51ad195ca033b6e
Merlin Beutlberger <m.beutlberger@sap.com>
2025-06-13 13:24:58 +0200

    feat(autofix): Fix basic deprecated Configuration APIs

--- src/linter/ui5Types/fix/collections/sapUiCoreFixes.ts
+++ src/linter/ui5Types/fix/collections/sapUiCoreFixes.ts
@@ -19,6 +19,61 @@ t.declareModule("sap/ui/core/Configuration", [
 			moduleName: "sap/base/i18n/Localization",
 			mustNotUseReturnValue: true,
 		})),
+		t.method("getAccessibility", accessExpressionFix({
+			moduleName: "sap/ui/core/ControlBehavior",
+			propertyAccess: "isAccessibilityEnabled",
+		})),
+		t.method("getActiveTerminologies", accessExpressionFix({
+			moduleName: "sap/base/i18n/Localization",
+			propertyAccess: "getActiveTerminologies",
+		})),
+		t.method("getAllowlistService", accessExpressionFix({
+			moduleName: "sap/ui/security/Security",
+			propertyAccess: "getAllowlistService",
+		})),
+		t.method("getAnimation", callExpressionFix({
+			moduleName: "sap/ui/core/ControlBehavior",
+			propertyAccess: "getAnimationMode",
+			// Note: The new API returns an enum value instead of a boolean, therefore
+			// migration is currently not possible if the return value is used
+			// This could be optimized with an advanced migration that detects how the return
+			// value is used and e.g. migrates to something like
+			// (getAnimationMode() !== sap.ui.core.Configuration.AnimationMode.none)
+			// Right now this migration probably wont apply for most cases
+			mustNotUseReturnValue: true,
+		})),
+		t.method("getAnimationMode", accessExpressionFix({
+			scope: FixScope.FirstChild,
+			moduleName: "sap/ui/core/ControlBehavior",
+		})),
+		t.method("getCalendarType", accessExpressionFix({
+			moduleName: "sap/base/i18n/Formatting",
+			propertyAccess: "getCalendarType",
+		})),
+		t.method("getCalendarWeekNumbering", accessExpressionFix({
+			moduleName: "sap/base/i18n/Formatting",
+			propertyAccess: "getCalendarWeekNumbering",
+		})),
+		t.method("getFrameOptions", accessExpressionFix({
+			moduleName: "sap/ui/security/Security",
+			propertyAccess: "getFrameOptions",
+		})),
+		t.method("getFormatLocale", callExpressionGeneratorFix({
+			moduleName: "sap/base/i18n/Formatting",
+			generator: (ctx, [moduleIdentifier]) => {
+				return `${moduleIdentifier}.getLanguageTag().toString()`;
+			},
+		})),
+		t.method("getLanguage", accessExpressionFix({
+			moduleName: "sap/base/i18n/Localization",
+			propertyAccess: "getLanguage",
+		})),
+		t.method("getLanguageTag", callExpressionGeneratorFix({
+			moduleName: "sap/base/i18n/Localization",
+			generator: (ctx, [moduleIdentifier]) => {
+				return `${moduleIdentifier}.getLanguageTag().toString()`;
+			},
+		})),
 		t.method("getLocale", callExpressionGeneratorFix({
 			moduleImports: [{
 				moduleName: "sap/ui/core/Locale",
@@ -29,6 +84,70 @@ t.declareModule("sap/ui/core/Configuration", [
 				return `new ${identifierNames[0]}(${identifierNames[1]}.getLanguageTag())`;
 			},
 		})),
+		t.method("getRTL", accessExpressionFix({
+			moduleName: "sap/base/i18n/Localization",
+			propertyAccess: "getRTL",
+		})),
+		t.method("getSAPLogonLanguage", accessExpressionFix({
+			moduleName: "sap/base/i18n/Localization",
+			propertyAccess: "getSAPLogonLanguage",
+		})),
+		t.method("getSecurityTokenHandlers", accessExpressionFix({
+			moduleName: "sap/ui/security/Security",
+			propertyAccess: "getSecurityTokenHandlers",
+		})),
+		t.method("getTheme", accessExpressionFix({
+			moduleName: "sap/ui/core/Theming",
+			propertyAccess: "getTheme",
+		})),
+		t.method("getTimezone", accessExpressionFix({
+			moduleName: "sap/base/i18n/Localization",
+			propertyAccess: "getTimezone",
+		})),
+		t.method("getUIDPrefix", accessExpressionFix({
+			moduleName: "sap/ui/base/ManagedObjectMetadata",
+			propertyAccess: "getUIDPrefix",
+		})),
+		t.method("getWhitelistService", accessExpressionFix({
+			moduleName: "sap/ui/security/Security",
+			propertyAccess: "getAllowlistService",
+		})),
+		t.method("setAnimationMode", accessExpressionFix({
+			moduleName: "sap/ui/core/ControlBehavior",
+			propertyAccess: "setAnimationMode",
+		})),
+		t.method("setCalendarType", callExpressionFix({
+			moduleName: "sap/base/i18n/Formatting",
+			propertyAccess: "setCalendarType",
+			mustNotUseReturnValue: true,
+		})),
+		t.method("setCalendarWeekNumbering", callExpressionFix({
+			moduleName: "sap/base/i18n/Formatting",
+			propertyAccess: "setCalendarWeekNumbering",
+			mustNotUseReturnValue: true,
+		})),
+		t.method("setFormatLocale", callExpressionFix({
+			moduleName: "sap/base/i18n/Formatting",
+			propertyAccess: "setLanguageTag",
+			mustNotUseReturnValue: true,
+		})),
+		t.method("setSecurityTokenHandlers", accessExpressionFix({
+			moduleName: "sap/ui/security/Security",
+			propertyAccess: "setSecurityTokenHandlers",
+		})),
+		t.method("setTheme", callExpressionFix({
+			moduleName: "sap/ui/core/Theming",
+			propertyAccess: "setTheme",
+			mustNotUseReturnValue: true,
+		})),
+		t.method("setTimezone", callExpressionFix({
+			moduleName: "sap/base/i18n/Localization",
+			propertyAccess: "setTimezone",
+			mustNotUseReturnValue: true,
+		})),
+
+		// TODO: enum sap/ui/core/AnimationMode
+		// https://sdk.openui5.org/api/module:sap/ui/core/AnimationMode
 	]),
 ]);
 
