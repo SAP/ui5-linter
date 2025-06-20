8ebafd063d342d7bd3f30f87557e0dc02a21f3dd
Max Reichmann <max.reichmann@sap.com>
2025-06-20 18:29:34 +0200

    test(Configuration): Update e2e tests

--- test/fixtures/e2e/runtime/test/qunit/Configuration.qunit.js
+++ test/fixtures/e2e/runtime/test/qunit/Configuration.qunit.js
@@ -6,7 +6,7 @@ sap.ui.define([
 ], (Configuration, CalendarType, CalendarWeekNumbering) => {
 	"use strict";
 
-	var globalConfiguration = sap.ui.getCore().getConfiguration();
+	const globalConfiguration = sap.ui.getCore().getConfiguration();
 
 	QUnit.module("Configuration");
 
@@ -15,51 +15,51 @@ sap.ui.define([
 		assert.strictEqual(typeof globalConfiguration.getAccessibility(), "boolean", "The result from getAccessibility() is a boolean");
 		assert.strictEqual(typeof sap.ui.getCore().getConfiguration().getAccessibility(), "boolean", "The result from getAccessibility() is a boolean");
 	});
-	
+
 	QUnit.test("getActiveTerminologies", function (assert) {
 		assert.ok(Array.isArray(Configuration.getActiveTerminologies()) || Configuration.getActiveTerminologies() === undefined, "Type is string[]|undefined");
 		assert.ok(Array.isArray(globalConfiguration.getActiveTerminologies()) || globalConfiguration.getActiveTerminologies() === undefined, "Type is string[]|undefined");
 		assert.ok(Array.isArray(sap.ui.getCore().getConfiguration().getActiveTerminologies()) || sap.ui.getCore().getConfiguration().getActiveTerminologies() === undefined, "Type is string[]|undefined");
 	});
-	
+
 	QUnit.test("getAllowlistService", function (assert) {
 		assert.strictEqual(typeof Configuration.getAllowlistService(), "string", "The result from getAllowlistService() is a string");
 		assert.strictEqual(typeof globalConfiguration.getAllowlistService(), "string", "The result from getAllowlistService() is a string");
 		assert.strictEqual(typeof sap.ui.getCore().getConfiguration().getAllowlistService(), "string", "The result from getAllowlistService() is a string");
 	});
-	
+
 	QUnit.test("getCalendarType", function (assert) {
-		var list = CalendarType;
+		const list = CalendarType;
 		assert.ok(list[Configuration.getCalendarType()], "The result from getCalendarType() is a CalendarType instance");
 		assert.ok(list[globalConfiguration.getCalendarType()], "The result from getCalendarType() is a CalendarType instance");
 		assert.ok(list[sap.ui.getCore().getConfiguration().getCalendarType()], "The result from getCalendarType() is a CalendarType instance");
 	});
-	
+
 	QUnit.test("getCalendarWeekNumbering", function (assert) {
-		var list = CalendarWeekNumbering;
+		const list = CalendarWeekNumbering;
 		assert.ok(list[Configuration.getCalendarWeekNumbering()], "The result from getCalendarWeekNumbering() is a CalendarWeekNumbering instance");
 		assert.ok(list[globalConfiguration.getCalendarWeekNumbering()], "The result from getCalendarWeekNumbering() is a CalendarWeekNumbering instance");
 		assert.ok(list[sap.ui.getCore().getConfiguration().getCalendarWeekNumbering()], "The result from getCalendarWeekNumbering() is a CalendarWeekNumbering instance");
 	});
-	
+
 	QUnit.test("getFrameOptions", function (assert) {
 		assert.strictEqual(typeof Configuration.getFrameOptions(), "string", "The result from getFrameOptions() is a string");
 		assert.strictEqual(typeof globalConfiguration.getFrameOptions(), "string", "The result from getFrameOptions() is a string");
 		assert.strictEqual(typeof sap.ui.getCore().getConfiguration().getFrameOptions(), "string", "The result from getFrameOptions() is a string");
 	});
-	
+
 	QUnit.test("getLanguage", function (assert) {
 		assert.strictEqual(typeof Configuration.getLanguage(), "string", "The result from getLanguage() is a string");
 		assert.strictEqual(typeof globalConfiguration.getLanguage(), "string", "The result from getLanguage() is a string");
 		assert.strictEqual(typeof sap.ui.getCore().getConfiguration().getLanguage(), "string", "The result from getLanguage() is a string");
 	});
-	
+
 	QUnit.test("getRTL", function (assert) {
 		assert.strictEqual(typeof Configuration.getRTL(), "boolean", "The result from getRTL() is a boolean");
 		assert.strictEqual(typeof globalConfiguration.getRTL(), "boolean", "The result from getRTL() is a boolean");
 		assert.strictEqual(typeof sap.ui.getCore().getConfiguration().getRTL(), "boolean", "The result from getRTL() is a boolean");
 	});
-	
+
 	QUnit.test("getSAPLogonLanguage", function (assert) {
 		assert.strictEqual(typeof Configuration.getSAPLogonLanguage(), "string", "The result from getSAPLogonLanguage() is a string");
 		assert.strictEqual(typeof globalConfiguration.getSAPLogonLanguage(), "string", "The result from getSAPLogonLanguage() is a string");
@@ -71,31 +71,37 @@ sap.ui.define([
 		assert.strictEqual(typeof globalConfiguration.getTheme(), "string", "The result from getTheme() is a string");
 		assert.strictEqual(typeof sap.ui.getCore().getConfiguration().getTheme(), "string", "The result from getTheme() is a string");
 	});
-	
+
 	QUnit.test("getTimezone", function (assert) {
 		assert.strictEqual(typeof Configuration.getTimezone(), "string", "The result from getTimezone() is a string");
 		assert.strictEqual(typeof globalConfiguration.getTimezone(), "string", "The result from getTimezone() is a string");
 		assert.strictEqual(typeof sap.ui.getCore().getConfiguration().getTimezone(), "string", "The result from getTimezone() is a string");
 	});
-	
+
 	QUnit.test("getUIDPrefix", function (assert) {
 		assert.strictEqual(typeof Configuration.getUIDPrefix(), "string", "The result from getUIDPrefix() is a string");
 		assert.strictEqual(typeof globalConfiguration.getUIDPrefix(), "string", "The result from getUIDPrefix() is a string");
 		assert.strictEqual(typeof sap.ui.getCore().getConfiguration().getUIDPrefix(), "string", "The result from getUIDPrefix() is a string");
 	});
-	
+
 	QUnit.test("getWhitelistService", function (assert) {
 		assert.strictEqual(typeof Configuration.getWhitelistService(), "string", "The result from getWhitelistService() is a string");
 		assert.strictEqual(typeof globalConfiguration.getWhitelistService(), "string", "The result from getWhitelistService() is a string");
 		assert.strictEqual(typeof sap.ui.getCore().getConfiguration().getWhitelistService(), "string", "The result from getWhitelistService() is a string");
 	});
-	
+
 	QUnit.test("getLanguageTag", function (assert) {
 		assert.strictEqual(typeof Configuration.getLanguageTag(), "string", "The result from getLanguageTag() is a string");
 		assert.strictEqual(typeof globalConfiguration.getLanguageTag(), "string", "The result from getLanguageTag() is a string");
 		assert.strictEqual(typeof sap.ui.getCore().getConfiguration().getLanguageTag(), "string", "The result from getLanguageTag() is a string");
 	});
-	
+
+	QUnit.test("getLocale", function (assert) {
+		assert.ok(Configuration.getLocale() instanceof sap.ui.core.Locale, "The result from getLocale() is of type sap.ui.core.Locale");
+		assert.ok(globalConfiguration.getLocale() instanceof sap.ui.core.Locale, "The result from getLocale() is of type sap.ui.core.Locale");
+		assert.ok(sap.ui.getCore().getConfiguration().getLocale()instanceof sap.ui.core.Locale, "The result from getLocale() is of type sap.ui.core.Locale");
+	});
+
 	QUnit.test("getFormatLocale", function (assert) {
 		assert.strictEqual(typeof Configuration.getFormatLocale(), "string", "The result from getFormatLocale() is a string");
 		assert.strictEqual(typeof globalConfiguration.getFormatLocale(), "string", "The result from getFormatLocale() is a string");
@@ -103,38 +109,41 @@ sap.ui.define([
 	});
 
 	QUnit.test("AnimationMode", function (assert) {
-		var list = Configuration.AnimationMode;
+		const list = Configuration.AnimationMode;
 		assert.ok(list[Configuration.getAnimationMode()], "The result from getAnimationMode() is an AnimationMode instance");
 		assert.ok(list[globalConfiguration.getAnimationMode()], "The result from getAnimationMode() is an AnimationMode instance");
 		assert.ok(list[sap.ui.getCore().getConfiguration().getAnimationMode()], "The result from getAnimationMode() is an AnimationMode instance");
-		
+
 		Configuration.setAnimationMode(Configuration.AnimationMode.minimal);
 		assert.strictEqual(globalConfiguration.getAnimationMode(), Configuration.AnimationMode.minimal, "The animation mode is set to minimal");
-		
+
 		globalConfiguration.setAnimationMode(globalConfiguration.AnimationMode.full);
 		assert.strictEqual(Configuration.getAnimationMode(), Configuration.AnimationMode.full, "The animation mode is set to full");
-		
+
 		sap.ui.getCore().getConfiguration().setAnimationMode(sap.ui.getCore().getConfiguration().AnimationMode.none);
 		assert.strictEqual(Configuration.getAnimationMode(), globalConfiguration.AnimationMode.none, "The animation mode is set to none");
 	});
-	
+
 	QUnit.test("SecurityTokenHandlers", function (assert) {
 		assert.ok(Array.isArray(Configuration.getSecurityTokenHandlers()), "Type is Array");
 		assert.ok(Array.isArray(globalConfiguration.getSecurityTokenHandlers()), "Type is Array");
 		assert.ok(Array.isArray(sap.ui.getCore().getConfiguration().getSecurityTokenHandlers()), "Type is Array");
-		
+
 		const handler1 = [() => new Promise((resolve) => { resolve("*Security token handler 1*"); })];
 		const handler2 = [() => new Promise((resolve) => { resolve("*Security token handler 2*"); })];
 		const handler3 = [() => new Promise((resolve) => { resolve("*Security token handler 3*"); })];
-		
+
 		Configuration.setSecurityTokenHandlers(handler1);
 		assert.deepEqual(globalConfiguration.getSecurityTokenHandlers(), handler1, "Security token handlers are set correctly");
-		
+
 		globalConfiguration.setSecurityTokenHandlers(handler2);
 		assert.deepEqual(Configuration.getSecurityTokenHandlers(), handler2, "Security token handlers are set correctly");
-		
+
 		sap.ui.getCore().getConfiguration().setSecurityTokenHandlers(handler3);
 		assert.deepEqual(Configuration.getSecurityTokenHandlers(), handler3, "Security token handlers are set correctly");
 	});
+
+	//TODO: Add tests for the complex migrations:
+	// ...
 
 });
