d8fd9b56690fe0f067ba52e94db358d6118e4bdf
Yavor Ivanov <yavor.ivanov@sap.com>
2025-07-02 11:17:33 +0300

    test: Fix negative test for no deprecated api

--- test/fixtures/linter/rules/NoDeprecatedApi/NoDeprecatedApi_negative.js
+++ test/fixtures/linter/rules/NoDeprecatedApi/NoDeprecatedApi_negative.js
@@ -1,14 +1,8 @@
 sap.ui.define([
-	"sap/m/upload/UploadSet",
 	"sap/ui/thirdparty/jquery",
 	"sap/m/library",
 	"sap/ui/test/matchers/Ancestor"
-], function(UploadSet, jQuery, mobileLib, Ancestor) {
-
-	var uploadSet = new UploadSet({
-		noDataText: "No data" // OK: Property 'noDataText' is only deprecated as of 1.121
-	});
-
+], function(jQuery, mobileLib, Ancestor) {
 	jQuery(document).on("touchstart mousedown", function() { // OK: Deprecated jQuery functions should not be checked
 		console.log("Hello World!");
 	});
@@ -18,5 +12,6 @@ sap.ui.define([
 	// Matchers are classes but return a function instead of an instance.
 	// This is a special case w.r.t. AST nodes, so it should be covered.
 	// There is no deprecated API usage in the following line.
+	const uploadSet = () => {};
 	new Ancestor("my-button")(uploadSet);
 });
